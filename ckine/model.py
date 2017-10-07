4db8d14db5d0ce9d405fad1fc1567a8c67f4e8f5
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2017-10-06 22:28:05 -0700

    Update model.py

--- ckine/model.py
+++ ckine/model.py
@@ -25,11 +25,6 @@ def dy_dt(y, t, IL2, k1fwd, k4fwd, k5rev, k6rev, k10rev, k11rev):
     k2rev = k2fwd * 144 # doi:10.1016/j.jmb.2004.04.038, 144 nM
     k3fwd = k1fwd / 10. # Very weak, > 50 uM. Voss, et al (1993). PNAS. 90, 2428–2432.
     k3rev = 50000 * k3fwd
-
-    # test to see that all three of the denominators in line 31 are non-zero
-    # print (k1rev)
-    # print (k6fwd)
-    # print (k3fwd)
     
     # To satisfy detailed balance these relationships should hold
     # _Based on initial assembly steps
