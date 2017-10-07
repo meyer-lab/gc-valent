7a6c8c851a260deea6ed92cb9e0be41a17b10c90
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2017-10-06 22:29:03 -0700

    Update model.py

--- ckine/model.py
+++ ckine/model.py
@@ -23,9 +23,9 @@ def dy_dt(y, t, IL2, k1fwd, k4fwd, k5rev, k6rev, k10rev, k11rev):
     k1rev = k1fwd * 10 # doi:10.1016/j.jmb.2004.04.038, 10 nM
     k2fwd = k1fwd
     k2rev = k2fwd * 144 # doi:10.1016/j.jmb.2004.04.038, 144 nM
-    k3fwd = k1fwd / 10. # Very weak, > 50 uM. Voss, et al (1993). PNAS. 90, 2428–2432.
+    k3fwd = k1fwd / 10.0 # Very weak, > 50 uM. Voss, et al (1993). PNAS. 90, 2428–2432.
     k3rev = 50000 * k3fwd
-    
+
     # To satisfy detailed balance these relationships should hold
     # _Based on initial assembly steps
     k4rev = k1fwd * k4fwd * k6rev * k3rev / k1rev / k6fwd / k3fwd
