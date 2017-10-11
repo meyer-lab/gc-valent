3be2d2808cb6bcf68adf84d74d948283052c676c
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2017-10-11 11:22:30 -0700

    bump

--- ckine/tests/test_model.py
+++ ckine/tests/test_model.py
@@ -8,13 +8,10 @@ class TestModel(unittest.TestCase):
     def setUp(self):
         print("Setup testing")
         
-    def test_equilibrium(self):
+    def test_conservation(self):
         ts = np.array([0.0, 100000.0])
         y0 = np.ones((10, ), dtype = np.float64)
         args = (1., 1., 1., 1., 1., 1., 0.5) # these 7 float values represent the inputs IL2 through k11rev
         y, fullout = odeint(dy_dt, y0, ts, args,
                     full_output = True, mxstep = 5000)
-        z = np.linalg.norm(dy_dt(y[1, :], 0, *args)) # have the sum of squares for all the dy_dt values be z
         
-        self.assertEqual((dy_dt(y[1, :], 0, *args)),10)
-        self.assertTrue(z < 1E-5)
