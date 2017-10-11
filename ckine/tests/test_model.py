81b69e0e420fbb8afee2d9c20ac29d2683a379d8
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2017-10-11 15:31:19 -0700

    Random numbers in setUp
    had to convert self.args to a tuple in order for them to be passed into dy_dt

--- ckine/tests/test_model.py
+++ ckine/tests/test_model.py
@@ -9,12 +9,12 @@ class TestModel(unittest.TestCase):
         print("Setup testing")
         self.ts = np.array([0.0, 100000.0])
         self.y0 = np.random.lognormal(0., 1., 10)
-        self.args = np.random.lognormal(0., 1., 7)
-        
+        self.args1 = list(np.random.lognormal(0., 1., 7))
+        self.args = tuple(self.args1)
+        # need to convert args from an array to a tuple of numbers
         
     def test_equilibrium(self):
         y, fullout = odeint(dy_dt, self.y0, self.ts, self.args,
                     full_output = True, mxstep = 5000)
         z = np.linalg.norm(dy_dt(y[1, :], 0, *self.args)) # have the sum of squares for all the dy_dt values be z
         self.assertTrue(z < 1E-5)
-        
