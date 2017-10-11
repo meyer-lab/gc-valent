447b328a314c94db3b6700346c3a17f812924ab5
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2017-10-11 11:23:08 -0700

    bump

--- ckine/tests/test_model.py
+++ ckine/tests/test_model.py
@@ -15,3 +15,15 @@ class TestModel(unittest.TestCase):
         y, fullout = odeint(dy_dt, y0, ts, args,
                     full_output = True, mxstep = 5000)
         
+       #Assign a value eq for the sum of amounts of receptors at equilirium
+        gc_eq = y[2] + y[5] + y[7] + y[8] + y[9]
+        IL2Ra_eq = y[0] + y[3] + y[6] + y[7] + y[9]
+        IL2Rb_eq = y[1] + y[4] + y[6] + y[8] + y[9]
+        
+        #Assign a value for the sum of amounts of each receptor at initial conditions
+        gc_initial = y0[2] + y0[5] + y0[7] + y0[8] + y0[9]
+        IL2Ra_initial = y0[0] + y0[3] + y0[6] + y0[7] + y0[9]
+        IL2Rb_initial = y0[1] + y0[4] + y0[6] + y0[8] + y0[9]
+        
+        #Check for conservation of gc
+        
