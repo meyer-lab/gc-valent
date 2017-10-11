fb5f5656ff4410c4e35911e97d1af6138bf971f4
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2017-10-10 18:24:30 -0700

    test_equilibrium with random inputs
    this unittest failed to compile but when I printed the output of the np.random.lognormal functions I got the correct number of random numbers so I think there's something in test_equilibrium that needs changing or I need to further specify the random numbers as floats.

--- ckine/tests/test_model.py
+++ ckine/tests/test_model.py
@@ -7,13 +7,14 @@ from scipy.integrate import odeint
 class TestModel(unittest.TestCase):
     def setUp(self):
         print("Setup testing")
+        self.ts = np.array([0.0, 100000.0])
+        self.y0 = np.random.lognormal(0., 1., 10)
+        self.args = np.random.lognormal(0., 1., 7)
+        
         
     def test_equilibrium(self):
-        ts = np.array([0.0, 100000.0])
-        y0 = np.ones((10, ), dtype = np.float64)
-        args = (1., 1., 1., 1., 1., 1., 0.5) # these 7 float values represent the inputs IL2 through k11rev
-        y, fullout = odeint(dy_dt, y0, ts, args,
+        y, fullout = odeint(dy_dt, self.y0, self.ts, self.args,
                     full_output = True, mxstep = 5000)
-        z = np.linalg.norm(dy_dt(y[1, :], 0, *args)) # have the sum of squares for all the dy_dt values be z
+        z = np.linalg.norm(dy_dt(y[1, :], 0, *self.args)) # have the sum of squares for all the dy_dt values be z
         self.assertTrue(z < 1E-5)
         
