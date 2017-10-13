0990d61e45a0eef13e02b22c37b93a178acbd7c0
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2017-10-12 19:24:49 -0700

    bump

--- ckine/model.py
+++ ckine/model.py
@@ -102,7 +102,7 @@ def dy_dt(y, t, IL2, IL15, k1fwd, k4fwd, k5rev, k6rev, k10rev, k11rev):
     dydt[10] = -k13fwd * IL15Ra * IL15 + k13rev * IL15_IL15Ra - k18fwd * IL15Ra * IL15_gc + k18rev * IL15_IL15Ra_gc - k20fwd * IL15Ra * IL15_IL2Rb_gc + k20rev * IL15_IL15Ra_IL2Rb_gc - k24fwd * IL15Ra * IL15_IL2Rb + k24rev * IL15_IL15Ra_IL2Rb
     dydt[11] = -k23fwd * IL15_IL15Ra * IL2Rb + k23rev * IL15_IL15Ra_IL2Rb - k16fwd * IL15_IL15Ra * gc + k16rev * IL15_IL15Ra_gc + k13fwd * IL15 * IL15Ra - k13rev * IL15_IL15Ra
     dydt[12] = -k24fwd * IL15_IL2Rb * IL15Ra + k24rev * IL15_IL15Ra_IL2Rb - k17fwd * IL15_IL2Rb * gc + k17rev * IL15_IL2Rb_gc + k14fwd * IL15 * IL2Rb - k14rev * IL15_IL2Rb 
-    dydt[13] = -k18fwd *IL15_gc * IL15Ra + k18rev * IL15_IL15Ra_gc - k19fwd * IL15_gc * IL2Rb + k19rev * IL15_IL2Rb_gc + k15fwd * IL15 * gc - k15rev * IL15_gc
+    dydt[13] = -k18fwd * IL15_gc * IL15Ra + k18rev * IL15_IL15Ra_gc - k19fwd * IL15_gc * IL2Rb + k19rev * IL15_IL2Rb_gc + k15fwd * IL15 * gc - k15rev * IL15_gc
     dydt[14] = -k22fwd * IL15_IL15Ra_IL2Rb * gc + k22rev * IL15_IL15Ra_IL2Rb_gc + k23fwd * IL15_IL15Ra * IL2Rb - k23rev * IL15_IL15Ra_IL2Rb + k24fwd * IL15_IL2Rb * IL15Ra - k24rev * IL15_IL15Ra_IL2Rb   
     dydt[15] = -k21fwd * IL15_IL15Ra_gc * IL2Rb + k21rev * IL15_IL15Ra_IL2Rb_gc + k16fwd * IL15_IL15Ra * gc - k16rev * IL15_IL15Ra_gc + k18fwd * IL15_gc * IL15Ra - k6rev * IL15_IL15Ra_gc
     dydt[16] = -k20fwd * IL15_IL2Rb_gc * IL15Ra + k20rev * IL15_IL15Ra_IL2Rb_gc + k17fwd * gc * IL15_IL2Rb - k17rev * IL15_IL2Rb_gc + k19fwd * IL15_gc * IL2Rb - k19rev * IL15_IL2Rb_gc
@@ -112,8 +112,4 @@ def dy_dt(y, t, IL2, IL15, k1fwd, k4fwd, k5rev, k6rev, k10rev, k11rev):
     dydt[2] = dydt[2] -k15fwd * IL15 * gc + k15rev * IL15_gc - k17fwd * IL15_IL2Rb * gc + k17rev * IL15_IL2Rb_gc - k16fwd * IL15_IL15Ra * gc + k16rev * IL15_IL15Ra_gc - k22fwd * IL15_IL15Ra_IL2Rb * gc + k10rev * IL15_IL15Ra_IL2Rb_gc
 
     
-    
-
-
-
     return dydt
