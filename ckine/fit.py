e08a52437ee34c9cfd49cc77a379751971f5a1dc
Aaron Meyer <a@ameyer.me>
2017-11-21 08:20:16 -0800

    Simplified some of the code layout.

--- ckine/fit.py
+++ ckine/fit.py
@@ -4,7 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import pandas as pds
-import theano
 from theano.compile.ops import as_op
 import theano.tensor as T
 from pymc3 import Model, Lognormal, Normal
@@ -31,27 +30,34 @@ def IL2_activity_values(y0, t, k4fwd, k5rev, k6rev):
     table = np.zeros((8,2))
     for ii in range (IL2s.shape[0]):
         activity[ii] = IL2_activity_input(y0, t, IL2s[ii], k4fwd, k5rev, k6rev)
-        table[ii, 0] = IL2s[ii]
-        table[ii, 1] = activity[ii]
+    
+    table[:, 0] = IL2s
+    table[:, 1] = activity
+
     return table
 
 
 def IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev):
     values = IL2_activity_values(y0, t, k4fwd, k5rev, k6rev)
     maximum = np.amax(values[:,1], 0) # find the max value in the second column for all rows
-    #print (maximum) 
     new_table = np.zeros((8,2))
-    x = np.zeros((8,1))
-    for ii in range (values.shape[0]):
-        new_table[ii, 0] = values[ii, 0] # IL2 values in first column are the same
-        new_table[ii, 1] = 100. * values[ii, 1] / maximum # activity values in second column are converted to percents relative to maximum
-        x[ii] = math.log10(new_table[ii,0]) # changing the x values to the log10(nM) values that were in the published graph
-    #plt.rcParams.update({'font.size': 8})
-    #plt.xlabel("IL2 concentration (log(nm))")
-    #plt.ylabel("percent activation of pSTAT")
-    #plt.scatter(x[:], new_table[:,1])
-    #plt.show()
+
+    new_table[:, 0] = values[:, 0] # IL2 values in first column are the same
+    new_table[:, 1] = 100. * values[:, 1] / maximum # activity values in second column are converted to percents relative to maximum
+    
     return new_table
+
+
+def plot_IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev):
+    new_table = IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev)
+
+    x = math.log10(new_table[:, 0]) # changing the x values to the log10(nM) values that were in the published graph
+
+    plt.rcParams.update({'font.size': 8})
+    plt.xlabel("IL2 concentration (log(nm))")
+    plt.ylabel("percent activation of pSTAT")
+    plt.scatter(x[:], new_table[:,1])
+    plt.show()
 
 
 @as_op(itypes=[T.dscalar, T.dscalar, T.dscalar], otypes=[T.dvector])
@@ -61,31 +67,14 @@ def IL2_sum_squared_distance(k4fwd, k5rev, k6rev):
 
     activity_table = IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev) # generates output from percent activity function
     data = pds.read_csv("./data/IL2_IL15_extracted_data.csv") # imports csv file into pandas array
-    # the following tries to perform calculation using pandas arrays
-    #data.columns = ['IL2_conc','h9_2Ra-','IL2_2Ra-','IL15_2Ra-','IL15_15Ra_2Ra-', 'h9_2Ra+','IL2_2Ra+','IL15_2Ra+','IL15_15Ra_2Ra+' ] #change the names of the pandas columns in data
-    #print (data['IL2_2Ra-'])
-    #print (activity_table[:,1])
-    #pandas_activity = pds.DataFrame({'conc':[activity_table[:,0]], 'act':[activity_table[:,1]]}) # converts percent activity data into pandas file in order to allow for subtraction
-    #differences_Ra- = pandas_activity['act'].sub(data['IL2_2Ra-'])
-    #print (type(pandas_activity['IL2_2Ra-'])) # this line is generating an error
     
     # trying to perform calculation using numpy arrays
     numpy_data = data.as_matrix() #the IL2_IL2Ra- data is within the 3rd column (index 2)
-    #print (numpy_data[:,2])
-    #print (activity_table[:,1])
-    diff_data = np.zeros((8,1))
-    diff_data[:,0] = numpy_data[:,6] - activity_table[:,1] # second column represents IL2_IL2Ra+ data
+
+    diff_data = numpy_data[:,6] - activity_table[:,1] # second column represents IL2_IL2Ra+ data
     # deciding to return diff_data for now
 
     return np.squeeze(diff_data)
-    #print (diff_data)
-    #squared_data = diff_data**2
-    #print (squared_data)
-    #sum_squared_dist = np.sum(squared_data, 0) # 0 indicates that we're summing on the 0 axis
-    #print (sum_squared_dist)
-    #root_sum_squared_dist = sum_squared_dist**0.5
-    #dist_final = np.asscalar(np.array([root_sum_squared_dist])) # convert the numpy array of one float to a scalar float
-    #return dist_final
 
 #print (IL2_sum_squared_distance([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.], 50., 1., 1., 1.))
 
