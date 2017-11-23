c53a32d4cb69323e40ed05114b91f6e6ed24ee88
Aaron Meyer <a@ameyer.me>
2017-11-22 21:04:09 -0800

    Fixed handling with class-based data loading. Redecorated with as_op

--- ckine/fit.py
+++ ckine/fit.py
@@ -24,7 +24,10 @@ def IL2_activity_input(y0, t, IL2, k4fwd, k5rev, k6rev):
 
 # this takes all the desired IL2 values we want to test and gives us the maximum activity value
 # IL2 values pretty much ranged from 5 x 10**-4 to 500 nm with 8 points in between
-def IL2_activity_values(y0, t, k4fwd, k5rev, k6rev):
+@as_op(itypes=[T.dscalar, T.dscalar, T.dscalar], otypes=[T.dmatrix])
+def IL2_activity_values(k4fwd, k5rev, k6rev):
+    y0 = np.array([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.])
+    t = 50.
     IL2s = np.logspace(-3.3, 2.7, 8) # 8 log-spaced values between our two endpoints
     activity = np.zeros(8)
     table = np.zeros((8,2))
@@ -37,19 +40,19 @@ def IL2_activity_values(y0, t, k4fwd, k5rev, k6rev):
     return table
 
 
-def IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev):
-    values = IL2_activity_values(y0, t, k4fwd, k5rev, k6rev)
-    maximum = np.amax(values[:,1], 0) # find the max value in the second column for all rows
-    new_table = np.zeros((8,2))
 
-    new_table[:, 0] = values[:, 0] # IL2 values in first column are the same
-    new_table[:, 1] = 100. * values[:, 1] / maximum # activity values in second column are converted to percents relative to maximum
+def IL2_percent_activity(k4fwd, k5rev, k6rev):
+    values = IL2_activity_values(k4fwd, k5rev, k6rev)
+    maximum = T.max(values[:,1], 0) # find the max value in the second column for all rows
+
+    new_table = T.stack((values[:, 0], 100. * values[:, 1] / maximum), axis=1) # IL2 values in first column are the same
+    # activity values in second column are converted to percents relative to maximum
     
     return new_table
 
 
 def plot_IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev):
-    new_table = IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev)
+    new_table = IL2_percent_activity(k4fwd, k5rev, k6rev)
 
     x = math.log10(new_table[:, 0]) # changing the x values to the log10(nM) values that were in the published graph
 
@@ -60,7 +63,7 @@ def plot_IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev):
     plt.show()
 
 
-@as_op(itypes=[T.dscalar, T.dscalar, T.dscalar], otypes=[T.dvector])
+
 
 class IL2_sum_squared_dist:
     
@@ -69,9 +72,7 @@ class IL2_sum_squared_dist:
         self.numpy_data = data.as_matrix() #the IL2_IL2Ra- data is within the 3rd column (index 2)
         
     def calc(self, k4fwd, k5rev, k6rev):
-        y0 = np.array([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.])
-        t = 50.
-        activity_table = IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev)
+        activity_table = IL2_percent_activity(k4fwd, k5rev, k6rev)
         diff_data = self.numpy_data[:,6] - activity_table[:,1]
         return np.squeeze(diff_data)
         
@@ -93,6 +94,9 @@ class IL2_sum_squared_dist:
 
 #print (IL2_sum_squared_distance([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.], 50., 1., 1., 1.))
 
+dst = IL2_sum_squared_dist()
+dst.load()
+
 M = pm.Model()
 
 with M:
@@ -100,7 +104,6 @@ with M:
     k5rev = pm.Lognormal('k5rev', mu=0, sd=3)
     k6rev = pm.Lognormal('k6rev', mu=0, sd=3)
     
-    dst = IL2_sum_squared_dist()
     Y = dst.calc(k4fwd, k5rev, k6rev)
     
     Y_obs = pm.Normal('fitD', mu=0, sd=T.std(Y), observed=Y)
