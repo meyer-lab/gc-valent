1a4fa5eb9fdc702398e0593caae8141f4c0ab437
Aaron Meyer <a@ameyer.me>
2018-02-12 09:04:48 -0800

    pymc3 has introduced multiprocess sampling

--- ckine/differencing_op.py
+++ ckine/differencing_op.py
@@ -1,7 +1,6 @@
 """
 Theano Op for using differencing for Jacobian calculation.
 """
-from concurrent.futures import ProcessPoolExecutor
 import numpy as np
 from theano.tensor import dot, dmatrix, dvector, Op
 
@@ -10,9 +9,9 @@ class centralDiff(Op):
     itypes = [dvector]
     otypes = [dvector]
 
-    def __init__(self, calcModel, parallel=False):
+    def __init__(self, calcModel):
         self.M = calcModel
-        self.dg = centralDiffGrad(calcModel, parallel)
+        self.dg = centralDiffGrad(calcModel)
 
     def perform(self, node, inputs, outputs):
         if np.any(np.greater(inputs[0], 1.0E4)):
@@ -31,14 +30,8 @@ class centralDiffGrad(Op):
     itypes = [dvector]
     otypes = [dmatrix]
 
-    def __init__(self, calcModel, parallel):
+    def __init__(self, calcModel):
         self.M = calcModel
-
-        # Setup process pool if desired
-        if parallel:
-            self.pool = ProcessPoolExecutor()
-        else:
-            self.pool = None
 
     def perform(self, node, inputs, outputs):
         # Find our current point
@@ -51,24 +44,10 @@ class centralDiffGrad(Op):
         if np.all(np.less(inputs[0], 1.0E4)):
             f0 = self.M.calc(x0)
 
-            if self.pool is None: # If we're not running in parallel
-                # Do all the calculations
-                for i in range(x0.size):
-                    dx = x0.copy()
-                    dx[i] = dx[i] + epsilon
-                    jac[i] = (self.M.calc(dx) - f0)/epsilon
-                    
-            else: # If we are running in parallel
-                output = list()
-
-                # Schedule all the calculations
-                for i in range(x0.size):
-                    dx = x0.copy()
-                    dx[i] = dx[i] + epsilon
-                    output.append(self.pool.submit(self.M.calc, dx))
-
-                # Process all the results
-                for i, item in enumerate(output):
-                    jac[i] = (item.result() - f0)/epsilon
+            # Do all the calculations
+            for i in range(x0.size):
+                dx = x0.copy()
+                dx[i] = dx[i] + epsilon
+                jac[i] = (self.M.calc(dx) - f0)/epsilon
 
         outputs[0][0] = np.transpose(jac)
