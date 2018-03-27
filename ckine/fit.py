e4d6e764825fe9438ffa470cd5211ce5e9a4a794
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2018-03-27 11:12:53 -0700

    added IL15 fitting from original "fit_merge" branch

--- ckine/fit.py
+++ ckine/fit.py
@@ -20,6 +20,18 @@ def IL2_activity_input(IL2, rxnRates, trafRates):
 
     return getTotalActiveCytokine(0, ys[0, :])
 
+def IL15_activity_input(IL15, rxnRates, trafRates):
+    """Takes in the reaction rates, traficking rates, and the amount of IL15 that you want to simulate with, and it runs the model odeint. """
+    rxnRates[1] = IL15
+
+    ts = np.linspace(1., 500, 2)
+
+    ys, retVal = runCkine(ts, rxnRates, trafRates)
+
+    if retVal < 0:
+        return -100
+
+    return getTotalActiveCytokine(1, ys[1, :])
 
 def IL2_convertRates(unkVec):
     """This takes in a vector of the values that we are fitting and it assigns them to the different reaction rates and ligand concentrations."""
@@ -47,6 +59,18 @@ def surf_IL2Rb(rxnRates, trafRates, IL2_conc):
     sIL2Rb = ys[:,1] # y[:,1] represents the surface IL2Rb value in fullModel
 
     return 10. * (sIL2Rb / sIL2Rb[0]) # % sIL2Rb relative to initial amount
+
+def IL15_convertRates(unkVec):
+    """This takes in a vector of the values that we are fitting and it assigns them to the different reaction rates and ligand concentrations."""
+    rxnRates = np.ones(17, dtype=np.float64)
+    rxnRates[4] = unkVec[0] # kfwd
+    rxnRates[7:12] = unkVec[3:8] # k15rev, k17rev, k18rev, k22rev, k23rev
+    rxnRates[0:4] = 0.0 # ligands
+
+    tfR = np.zeros(11, dtype=np.float64)
+    tfR[0:9] = unkVec[8:17] # last 4 are expr of IL2Ra, IL2Rb, gc, IL15Ra
+
+    return (rxnRates, tfR)
 
 
 class IL2Rb_trafficking:
@@ -80,7 +104,7 @@ class IL2Rb_trafficking:
 # this takes all the desired IL2 values we want to test and gives us the maximum activity value
 # IL2 values pretty much ranged from 5 x 10**-4 to 500 nm with 8 points in between
 # need the theano decorator to get around the fact that there are if-else statements when running odeint but
-#  we don't necessarily know the values for the rxn rates when we call our model
+# we don't necessarily know the values for the rxn rates when we call our model
 class IL2_sum_squared_dist:
     def __init__(self):
         """This loads the experiment data and saves it as a member matrix and it also makes a vector of the IL2 concentrations that we are going to take care of."""
@@ -113,12 +137,41 @@ class IL2_sum_squared_dist:
 
         # value we're trying to minimize is the distance between the y-values on points of the graph that correspond to the same IL2 values
         return self.fit_data - actVec
+    
+    
+class IL15_sum_squared_dist:
+    def __init__(self):
+        """This loads the experiment data and saves it as a member matrix and it also makes a vector of the IL15 concentrations that we are going to take care of."""
+        path = os.path.dirname(os.path.abspath(__file__))
+        data = pds.read_csv(os.path.join(path, "./data/IL2_IL15_extracted_data.csv")) # imports csv file into pandas array
+        self.numpy_data = data.as_matrix() #the IL15_IL2Ra- data is within the 4th column (index 3)
+        self.IL15s = np.logspace(-3.3, 2.7, 8) # 8 log-spaced values between our two endpoints
+        self.concs = len(self.IL15s)
+        self.fit_data = np.concatenate((self.numpy_data[:, 7], self.numpy_data[:, 3]))
 
+    def calc(self, unkVec):
+        """Simulate the experiment with IL15. It is making a list of promises which will be calculated and returned as output."""
+        # Convert the vector of values to dicts
+        rxnRates, tfR = IL15_convertRates(unkVec)
+
+        # IL2Ra- cells have same IL15 activity, so we can just reuse same solution
+        actVec = np.zeros(self.concs, dtype=np.float64)
+
+        # Loop over concentrations of IL15
+        for ii, ILc in enumerate(self.IL15s):
+            actVec[ii] = IL15_activity_input(ILc, rxnRates.copy(), tfR)
+
+        # Normalize to the maximal activity, put together into one vector
+        actVec = np.concatenate((actVec / np.max(actVec), actVec / np.max(actVec)))
+        # value we're trying to minimize is the distance between the y-values on points of the graph that correspond to the same IL2 values
+        return self.fit_data - actVec
+    
 
 class build_model:
     """Going to load the data from the CSV file at the very beginning of when build_model is called... needs to be separate member function to avoid uploading file thousands of times."""
     def __init__(self):
-        self.dst = IL2_sum_squared_dist()
+        self.dst2 = IL2_sum_squared_dist()
+        self.dst15 = IL15_sum_squared_dist()
         self.IL2Rb = IL2Rb_trafficking()
         self.M = self.build()
 
@@ -127,23 +180,24 @@ class build_model:
         M = pm.Model()
 
         with M:
-            rxnrates = pm.Lognormal('rxn', sd=1., shape=3, testval=[0.1, 0.1, 0.1])
+            rxnrates = pm.Lognormal('rxn', sd=1., shape=8, testval=[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]) # first 3 are IL2, second 5 are IL15, kfwd is first element (used in both 2&15)
             endo_activeEndo = pm.Lognormal('endo', mu=np.log(0.1), sd=1., shape=2, testval=[0.1, 0.1])
             kRec_kDeg = pm.Lognormal('kRec_kDeg', mu=np.log(0.1), sd=1., shape=2, testval=[0.1, 0.1])
-            Rexpr = pm.Lognormal('IL2Raexpr', sd=1., shape=3, testval=[1., 1., 1.])
+            Rexpr = pm.Lognormal('IL2Raexpr', sd=1., shape=4, testval=[1., 1., 1., 1.]) # Expression: IL2Ra, IL2Rb, gc, IL15Ra
             sortF = pm.Beta('sortF', alpha=2, beta=7, testval=0.1)
 
             unkVec = T.concatenate((rxnrates, endo_activeEndo, T.stack(sortF), kRec_kDeg, Rexpr))
 
-            Y = centralDiff(self.dst)(unkVec) # fitting the data based on dst.calc for the given parameters
-
+            Y_2 = centralDiff(self.dst2)(unkVec) # fitting the data based on dst2.calc for the given parameters
+            Y_15 = centralDiff(self.dst15)(unkVec) # fitting the data based on dst15.calc for the given parameters
             Y_int = centralDiff(self.IL2Rb)(unkVec) # fitting the data based on dst.calc for the given parameters
 
-            pm.Deterministic('Y', Y) # this line allows us to see the traceplots in read_fit_data.py... it lets us know if the fitting process is working
-
+            pm.Deterministic('Y_2', Y_2) # this line allows us to see the traceplots in read_fit_data.py... it lets us know if the fitting process is working
+            pm.Deterministic('Y_15', Y_15)
             pm.Deterministic('Y_int', Y_int)
 
-            pm.Normal('fitD', sd=0.1, observed=Y) # TODO: Find an empirical value for the SEM
+            pm.Normal('fitD_2', sd=0.1, observed=Y_2) # TODO: Find an empirical value for the SEM
+            pm.Normal('fitD_15', sd=0.1, observed=Y_15)
             pm.Normal('fitD_int', sd=0.1, observed=Y_int) # TODO: Find an empirical value for the SEM
 
             # Save likelihood
