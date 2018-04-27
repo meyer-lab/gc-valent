c59f24e8ccdcc3a080ec9e518ddeab961b6c475c
Aaron Meyer <a@ameyer.me>
2018-04-26 19:01:34 -0700

    Numpy floats have to be wrapped in ctype object.

--- ckine/model.py
+++ ckine/model.py
@@ -106,7 +106,7 @@ def jacobian(y, t, rxn):
     
     yOut = np.zeros((26,26)) # size of the surface Jacobian matrix
     
-    libb.jacobian_C(y.ctypes.data_as(ct.POINTER(ct.c_double)), t,
+    libb.jacobian_C(y.ctypes.data_as(ct.POINTER(ct.c_double)), ct.c_double(t),
                 yOut.ctypes.data_as(ct.POINTER(ct.c_double)), rxn.ctypes.data_as(ct.POINTER(ct.c_double)))
     
     return yOut 
@@ -118,7 +118,7 @@ def fullJacobian(y, t, rxn): # will eventually have to add tfR as an argument on
     
     yOut = np.zeros((56,56)) # size of the full Jacobian matrix
     
-    libb.fullJacobian_C(y.ctypes.data_as(ct.POINTER(ct.c_double)), t,
+    libb.fullJacobian_C(y.ctypes.data_as(ct.POINTER(ct.c_double)), ct.c_double(t),
                 yOut.ctypes.data_as(ct.POINTER(ct.c_double)), rxn.ctypes.data_as(ct.POINTER(ct.c_double)))
     
     return yOut 
