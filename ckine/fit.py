8b367ef02cdd78e7fad15dbaa2a8fc195a35363a
Aaron Meyer <a@ameyer.me>
2018-05-14 16:02:19 -0700

    Some tweaks to try and make the fitting work more reliably.

--- ckine/fit.py
+++ ckine/fit.py
@@ -92,12 +92,12 @@ class build_model:
         M = pm.Model()
 
         with M:
-            kfwd = pm.Lognormal('kfwd', mu=np.log(0.00001), sd=0.2)
+            kfwd = pm.Lognormal('kfwd', mu=np.log(0.00001), sd=0.1)
             rxnrates = pm.Lognormal('rxn', mu=np.log(0.01), sd=1., shape=9) # first 3 are IL2, second 5 are IL15, kfwd is first element (used in both 2&15)
             endo_activeEndo = pm.Lognormal('endo', mu=np.log(0.1), sd=1., shape=2)
             kRec_kDeg = pm.Lognormal('kRec_kDeg', mu=np.log(0.1), sd=1., shape=2)
             Rexpr = pm.Lognormal('IL2Raexpr', sd=1., shape=4) # Expression: IL2Ra, IL2Rb, gc, IL15Ra
-            sortF = pm.Beta('sortF', alpha=2, beta=7, testval=0.1)
+            sortF = pm.Beta('sortF', alpha=2, beta=7, testval=0.1)*0.9
 
             ligands = T.zeros(4, dtype=np.float64)
 
