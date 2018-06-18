330621f2ccff3a1b47bc696e0d531776ba253f73
adamcweiner <adamcweiner@gmail.com>
2018-06-18 11:48:12 -0700

    changed name of k4rev and k5rev again

--- ckine/fit.py
+++ ckine/fit.py
@@ -92,7 +92,7 @@ class build_model:
 
         with M:
             kfwd = pm.Lognormal('kfwd', mu=np.log(0.0001), sd=0.1)
-            k9rev = pm.Lognormal('k9rev', mu=np.log(0.1), sd=0.1, observed=((12.0 * rxnrates[1] / 1.5) * (63.0 * rxnrates[1] / 1.5) / rxnrates[0])) # rxnrates[0] = k4rev, rxnrates[1] = k5rev
+            k9rev = pm.Lognormal('k9rev', mu=np.log(0.1), sd=0.1, observed=((12.0 * k5rev / 1.5) * (63.0 * k5rev / 1.5) / k4rev)) # rxnrates[0] = k4rev, rxnrates[1] = k5rev
             rxnrates = pm.Lognormal('rxn', mu=np.log(0.1), sd=0.1, shape=8) # first 3 are IL2, second 5 are IL15, kfwd is first element (used in both 2&15)
             endo_activeEndo = pm.Lognormal('endo', mu=np.log(0.1), sd=0.1, shape=2)
             kRec_kDeg = pm.Lognormal('kRec_kDeg', mu=np.log(0.1), sd=0.1, shape=2)
