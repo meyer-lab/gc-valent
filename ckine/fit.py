5b0900a34c89127b71226441f0fa0b5774c1dfe8
adamcweiner <adamcweiner@gmail.com>
2018-06-19 15:44:07 -0700

    bump

--- ckine/fit.py
+++ ckine/fit.py
@@ -112,7 +112,7 @@ class build_model:
         M = pm.Model()
 
         with M:
-            kfwd = pm.Lognormal('kfwd', mu=np.log(0.0001), sd=0.1)
+            kfwd = pm.Lognormal('kfwd', mu=np.log(0.00001), sd=0.1)
             rxnrates = pm.Lognormal('rxn', mu=np.log(0.1), sd=0.1, shape=8) # first 3 are IL2, second 5 are IL15, kfwd is first element (used in both 2&15)
             k9rev = ((12.0 * rxnrates[1] / 1.5) * (63.0 * rxnrates[1] / 1.5) / rxnrates[0]) # rxnrates[0] = k4rev, rxnrates[1] = k5rev
             # k20rev = pm.Lognormal('k20rev', mu=np.log(0.1), sd=0.1, observed=((rxnrates[4] * ((0.60 * 0.065) * rxnrates[5] / (0.60 * 438)) / rxnrates[3])))
