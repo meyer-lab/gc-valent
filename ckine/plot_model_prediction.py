2f167eb92a8a5b144942bc42e6ce656bb7916af1
adamcweiner <adamcweiner@gmail.com>
2018-06-19 16:27:01 -0700

    finished IL2Rb_surf calculations

--- ckine/plot_model_prediction.py
+++ ckine/plot_model_prediction.py
@@ -21,28 +21,72 @@ class surf_IL2Rb:
         unkVec = np.array([0., 0., 0., 0., 8.33E-05, 0.115818087, 0.128608864, 0.089890767, 0.098851823, 0.106913961, 0.107722847, 0.117471386, 0.114016182, 0.095431878, 0.101391307, 0.340647077, 0.090746998, 0.097587796, 1.013323342, 0.925694209, 0.918209005, 0.958374837, 0., 0.])
 
         # set IL2 concentrations
-        unkVec_1 = unkVec.copy()
-        unkVec_1[0] = 1.
+        unkVec_IL2_1 = unkVec.copy()
+        unkVec_IL2_1[0] = 1.
         
-        unkVec_500 = unkVec.copy()
-        unkVec_500[0] = 500.
+        unkVec_IL2_500 = unkVec.copy()
+        unkVec_IL2_500[0] = 500.
         
         # set IL2Ra- values
-        unkVecIL2RaMinus_1 = unkVec_1.copy()
-        unkVecIL2RaMinus_1[18] = 0.0
+        unkVecIL2RaMinus_IL2_1 = unkVec_IL2_1.copy()
+        unkVecIL2RaMinus_IL2_1[18] = 0.0
         
-        unkVecIL2RaMinus_500 = unkVec_500.copy()
-        unkVecIL2RaMinus_500[18] = 0.0
+        unkVecIL2RaMinus_IL2_500 = unkVec_IL2_500.copy()
+        unkVecIL2RaMinus_IL2_500[18] = 0.0
         
-        # calculate 
-        a_yOut, a_retVal = runCkineU(self.ts, unkVec_1)
-        b_yOut, b_retVal = runCkineU(self.ts, unkVec_500)
-        c_yOut, c_retVal = runCkineU(self.ts, unkVecIL2RaMinus_1)
-        d_yOut, d_retVal = runCkineU(self.ts, unkVecIL2RaMinus_500)
+        # set IL15 concentrations
+        unkVec_IL15_1 = unkVec.copy()
+        unkVec_IL15_1[1] = 1.
         
+        unkVec_IL15_500 = unkVec.copy()
+        unkVec_IL15_500[1] = 500.
+        
+        # set IL2Ra- values
+        unkVecIL2RaMinus_IL15_1 = unkVec_IL15_1.copy()
+        unkVecIL2RaMinus_IL15_1[18] = 0.0
+        
+        unkVecIL2RaMinus_IL15_500 = unkVec_IL15_500.copy()
+        unkVecIL2RaMinus_IL15_500[18] = 0.0
+        
+        # calculate IL2 stimulation
+        a_yOut, a_retVal = runCkineU(self.ts, unkVec_IL2_1)
+        b_yOut, b_retVal = runCkineU(self.ts, unkVec_IL2_500)
+        c_yOut, c_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL2_1)
+        d_yOut, d_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL2_500)
+        
+        # calculate IL15 stimulation
+        e_yOut, e_retVal = runCkineU(self.ts, unkVec_IL15_1)
+        f_yOut, f_retVal = runCkineU(self.ts, unkVec_IL15_500)
+        g_yOut, g_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL15_1)
+        h_yOut, h_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL15_500) 
+        
+        # Condense to just IL2Rb
+        condense = np.zeros(48)
+        condense[1] = 1
+        # species in IL2 family that contain IL2Rb
+        condense[4] = 1
+        condense[5] = 1
+        condense[7] = 1
+        condense[8] = 1
+        # species in IL15 family that contain IL2Rb
+        condense[11] = 1
+        condense[12] = 1
+        condense[14] = 1
+        condense[15] = 1
+        
+        a = np.dot(a_yOut, condense)
+        b = np.dot(b_yOut, condense)
+        c = np.dot(c_yOut, condense)
+        d = np.dot(d_yOut, condense)
+        e = np.dot(e_yOut, condense)
+        f = np.dot(f_yOut, condense)
+        g = np.dot(g_yOut, condense)
+        h = np.dot(h_yOut, condense)
+        
+        print(a.shape)
         print(a_yOut.shape)
 
-        return (a / a[0], b / b[0], c / c[0], d / d[0])
+        return (a / a[0], b / b[0], c / c[0], d / d[0], e / e[0], f / f[0], g / g[0], h / h[0])
 
     def calc_2(self):
         "This function uses an unkVec from fitting surface IL2Rb and IL2 pSTAT data. This fitting process had high error so the return values mightly highly differ from the experimental figures "
