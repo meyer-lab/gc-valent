e45a6fef1cc6049f270f46c41631cebe2e338dcc
adamcweiner <adamcweiner@gmail.com>
2018-06-19 11:51:03 -0700

    add framework for model prediction overlay

---
+++ ckine/plot_model_prediction.py
@@ -0,0 +1,81 @@
+import theano.tensor as T, os
+from os.path import join
+from theano import shared
+import numpy as np, pandas as pds
+from .model import getActiveSpecies
+from .differencing_op import runCkineOp, runCkineKineticOp
+
+class surf_IL2Rb: 
+    def __init__(self):
+        # times from experiment are hard-coded into this function
+        self.ts = np.array([0., 2., 5., 15., 30., 60., 90.])
+
+        # Condense to just IL2Rb
+        self.condense = np.zeros(48)
+        self.condense[1] = 1
+
+    def calc_1(self):
+        "This function uses an unkVec from fitting surface IL2Rb data only. This data should closely match the experimental figures"
+        
+        # posteriors taken from line 170 of fitting_IL2Rb_only.csv
+        unkVec = np.array([0., 0., 0., 0., 8.33E-05, 0.115818087, 0.128608864, 0.089890767, 0.098851823, 0.106913961, 0.107722847, 0.117471386, 0.114016182, 0.095431878, 0.101391307, 0.340647077, 0.090746998, 0.097587796, 1.013323342, 0.925694209, 0.918209005, 0.958374837, 0., 0.])
+
+        KineticOp = runCkineKineticOp(self.ts, self.condense)
+
+        unkVecIL2RaMinus = T.set_subtensor(unkVec[18], 0.0) # Set IL2Ra to zero
+
+        a = KineticOp(T.set_subtensor(unkVec[0], 1.)) # col 2 of numpy_data has all the 1nM IL2Ra+ data
+        b = KineticOp(T.set_subtensor(unkVec[0], 500.)) # col 6 of numpy_data has all the 500 nM IL2Ra+ data
+        c = KineticOp(T.set_subtensor(unkVecIL2RaMinus[0], 1.)) # col 2 of numpy_data2 has all the 1nM IL2Ra- data
+        d = KineticOp(T.set_subtensor(unkVecIL2RaMinus[0], 500.)) # col 6 of numpy_data2 has all the 500 nM IL2Ra- data
+
+        return (a / a[0], b / b[0], c / c[0], d / d[0])
+
+    def calc_2(self):
+        "This function uses an unkVec from fitting surface IL2Rb and IL2 pSTAT data. This fitting process had high error so the return values mightly highly differ from the experimental figures "
+        
+        # posteriors taken from line 270 of full-fitting-no-IL15-hard-bound.csv
+        unkVec = np.array([0., 0., 0., 0., 0.00012865, 0.158037088, 0.285547466, 0.102937877, 0.399706229, 0.111318564, 0.061811833, 0.042950052, 0.283564271, 0.088275756, 0.08486402, 0.399519643 0.05414861, 0.253249545, 0.509200117, 0.537992752, 0.667527049, 3.852037782, 0., 0.])
+        
+        ineticOp = runCkineKineticOp(self.ts, self.condense)
+
+        unkVecIL2RaMinus = T.set_subtensor(unkVec[18], 0.0) # Set IL2Ra to zero
+
+        a = KineticOp(T.set_subtensor(unkVec[0], 1.)) # col 2 of numpy_data has all the 1nM IL2Ra+ data
+        b = KineticOp(T.set_subtensor(unkVec[0], 500.)) # col 6 of numpy_data has all the 500 nM IL2Ra+ data
+        c = KineticOp(T.set_subtensor(unkVecIL2RaMinus[0], 1.)) # col 2 of numpy_data2 has all the 1nM IL2Ra- data
+        d = KineticOp(T.set_subtensor(unkVecIL2RaMinus[0], 500.)) # col 6 of numpy_data2 has all the 500 nM IL2Ra- data
+
+        return (a / a[0], b / b[0], c / c[0], d / d[0])
+    
+class pstat:
+    def __init__(self):
+        self.cytokC = np.logspace(-3.3, 2.7, 8) # 8 log-spaced values between our two endpoints
+    
+        npactivity = getActiveSpecies().astype(np.float64)
+        self.activity = shared(np.concatenate((npactivity, 0.5*npactivity, np.zeros(4)))) # 0.5 is because its the endosome
+        
+    def calc_1(self):
+        # posteriors taken from line 270 of full-fitting-no-IL15-hard-bound.csv
+        # order of elements reagganged to match unkVec in fit.py
+        unkVec = np.array([0., 0., 0., 0., 0.00012865, 0.158037088, 0.285547466, 0.102937877, 0.399706229, 0.111318564, 0.061811833, 0.042950052, 0.283564271, 0.088275756, 0.08486402, 0.399519643 0.05414861, 0.253249545, 0.509200117, 0.537992752, 0.667527049, 3.852037782, 0., 0.])
+        
+        # IL2Ra- cells have same IL15 activity, so we can just reuse same solution
+        Op = runCkineOp(ts=np.array(500.))
+
+        # Loop over concentrations of IL15
+        actVec = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVec[1], x))), self.cytokC))) # Change condensation here for activity
+
+        # Loop over concentrations of IL2
+        actVecIL2 = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVec[0], x))), self.cytokC)))
+
+        unkVecIL2RaMinus = T.set_subtensor(unkVec[18], 0.0) # Set IL2Ra to zero
+
+        # Loop over concentrations of IL2, IL2Ra-/-
+        actVecIL2RaMinus = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVecIL2RaMinus[0], x))), self.cytokC)))
+
+        # Normalize to the maximal activity, put together into one vector
+        # actVec = T.concatenate((actVec / T.max(actVec), actVec / T.max(actVec), actVecIL2 / T.max(actVecIL2), actVecIL2RaMinus / T.max(actVecIL2RaMinus)))
+        actVec = T.concatenate((actVecIL2 / T.max(actVecIL2), actVecIL2RaMinus / T.max(actVecIL2RaMinus)))
+        # value we're trying to minimize is the distance between the y-values on points of the graph that correspond to the same IL2 values
+        return actVec
