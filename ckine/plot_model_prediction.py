7fdde90bd2e72efc7e82e557d0a8ca146ade773c
adamcweiner <adamcweiner@gmail.com>
2018-06-20 14:51:55 -0700

    turn unkVec into an input parameter for calc functions

--- ckine/plot_model_prediction.py
+++ ckine/plot_model_prediction.py
@@ -3,7 +3,7 @@ from .model import getActiveSpecies, runCkineU
 
 
 class surf_IL2Rb: 
-    "the only difference between calc_1 and calc_2 is the values in the unkVec that are passed in"
+    "Generate values to match the surface IL2Rb measurements used in fitting"
     def __init__(self):
         # times from experiment are hard-coded into this function
         self.ts = np.array([0., 2., 5., 15., 30., 60., 90.])
@@ -12,83 +12,10 @@ class surf_IL2Rb:
         self.condense = np.zeros(48)
         self.condense[1] = 1
 
-    def calc_1(self):
-        "This function uses an unkVec from fitting surface IL2Rb data only. This data should closely match the experimental figures"
+    def calc(self, unkVec):
+        "This function uses an unkVec that has the same elements as the unkVec in fit.py"
         
-        # posteriors taken from line 170 of fitting_IL2Rb_only.csv
-        # NOTE: these results were fit to IL2 stimulation only
-        unkVec = np.array([0., 0., 0., 0., 8.33E-05, 0.115818087, 0.128608864, 0.089890767, 0.098851823, 0.106913961, 0.107722847, 0.117471386, 0.114016182, 0.095431878, 0.101391307, 0.340647077, 0.090746998, 0.097587796, 1.013323342, 0.925694209, 0.918209005, 0.958374837, 0., 0.])
-
-        # set IL2 concentrations
-        unkVec_IL2_1 = unkVec.copy()
-        unkVec_IL2_1[0] = 1.
-        
-        unkVec_IL2_500 = unkVec.copy()
-        unkVec_IL2_500[0] = 500.
-        
-        # set IL2Ra- values
-        unkVecIL2RaMinus_IL2_1 = unkVec_IL2_1.copy()
-        unkVecIL2RaMinus_IL2_1[18] = 0.0
-        
-        unkVecIL2RaMinus_IL2_500 = unkVec_IL2_500.copy()
-        unkVecIL2RaMinus_IL2_500[18] = 0.0
-        
-        # set IL15 concentrations
-        unkVec_IL15_1 = unkVec.copy()
-        unkVec_IL15_1[1] = 1.
-        
-        unkVec_IL15_500 = unkVec.copy()
-        unkVec_IL15_500[1] = 500.
-        
-        # set IL2Ra- values
-        unkVecIL2RaMinus_IL15_1 = unkVec_IL15_1.copy()
-        unkVecIL2RaMinus_IL15_1[18] = 0.0
-        
-        unkVecIL2RaMinus_IL15_500 = unkVec_IL15_500.copy()
-        unkVecIL2RaMinus_IL15_500[18] = 0.0
-        
-        # calculate IL2 stimulation
-        a_yOut, a_retVal = runCkineU(self.ts, unkVec_IL2_1)
-        b_yOut, b_retVal = runCkineU(self.ts, unkVec_IL2_500)
-        c_yOut, c_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL2_1)
-        d_yOut, d_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL2_500)
-        
-        # calculate IL15 stimulation
-        e_yOut, e_retVal = runCkineU(self.ts, unkVec_IL15_1)
-        f_yOut, f_retVal = runCkineU(self.ts, unkVec_IL15_500)
-        g_yOut, g_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL15_1)
-        h_yOut, h_retVal = runCkineU(self.ts, unkVecIL2RaMinus_IL15_500) 
-        
-        # Condense to just IL2Rb
-        condense = np.zeros(48)
-        condense[1] = 1
-        # species in IL2 family that contain IL2Rb
-        condense[4] = 1
-        condense[5] = 1
-        condense[7] = 1
-        condense[8] = 1
-        # species in IL15 family that contain IL2Rb
-        condense[11] = 1
-        condense[12] = 1
-        condense[14] = 1
-        condense[15] = 1
-        
-        a = np.dot(a_yOut, condense)
-        b = np.dot(b_yOut, condense)
-        c = np.dot(c_yOut, condense)
-        d = np.dot(d_yOut, condense)
-        e = np.dot(e_yOut, condense)
-        f = np.dot(f_yOut, condense)
-        g = np.dot(g_yOut, condense)
-        h = np.dot(h_yOut, condense)
-
-        return (a / a[0], b / b[0], c / c[0], d / d[0], e / e[0], f / f[0], g / g[0], h / h[0])
-
-    def calc_2(self):
-        "This function uses an unkVec from fitting surface IL2Rb and IL2 pSTAT data. This fitting process had high error so the return values mightly highly differ from the experimental figures "
-        
-        # posteriors taken from line 270 of full-fitting-no-IL15-hard-bound.csv
-        unkVec = np.array([0., 0., 0., 0., 0.00012865, 0.158037088, 0.285547466, 0.102937877, 0.399706229, 0.111318564, 0.061811833, 0.042950052, 0.283564271, 0.088275756, 0.08486402, 0.399519643, 0.05414861, 0.253249545, 0.509200117, 0.537992752, 0.667527049, 3.852037782, 0., 0.])
+        assert(unkVec.size == 24)
         
         # set IL2 concentrations
         unkVec_IL2_1 = unkVec.copy()
@@ -154,8 +81,10 @@ class surf_IL2Rb:
         h = np.dot(h_yOut, condense)
 
         return (a / a[0], b / b[0], c / c[0], d / d[0], e / e[0], f / f[0], g / g[0], h / h[0])
+
     
 class pstat:
+    "Generate values to match the pSTAT5 measurements used in fitting"
     def __init__(self):
         self.cytokC = np.logspace(-3.3, 2.7, 8) # 8 log-spaced values between our two endpoints
     
@@ -163,10 +92,10 @@ class pstat:
         self.activity = np.concatenate((npactivity, 0.5*npactivity, np.zeros(4))) # 0.5 is because its the endosome
         self.ts = np.array([500.])
         
-    def calc_1(self):
-        # posteriors taken from line 270 of full-fitting-no-IL15-hard-bound.csv
-        # order of elements reagganged to match unkVec in fit.py
-        vec = np.array([0., 0., 0., 0., 0.00012865, 0.158037088, 0.285547466, 0.102937877, 0.399706229, 0.111318564, 0.061811833, 0.042950052, 0.283564271, 0.088275756, 0.08486402, 0.399519643, 0.05414861, 0.253249545, 0.509200117, 0.537992752, 0.667527049, 3.852037782, 0., 0.])
+    def calc(self, unkVec):
+        "This function uses an unkVec that has the same elements as the unkVec in fit.py"
+        
+        assert(unkVec.size == 24)
         
         # loop over concentrations of IL2
         unkVec_IL2 = np.zeros((24, 8))
@@ -176,7 +105,7 @@ class pstat:
         actVec_IL2 = np.zeros((8))
         actVec_IL2_IL2Raminus = actVec_IL2.copy()
         for ii in range(0,8):
-            unkVec_IL2[:, ii] = vec.copy()
+            unkVec_IL2[:, ii] = unkVec.copy()
             unkVec_IL2[0, ii] = self.cytokC[ii]
             
             unkVec_IL2_IL2Raminus[:,ii] = unkVec_IL2[:,ii].copy()
@@ -187,13 +116,13 @@ class pstat:
             IL2_yOut_IL2Raminus[ii,:], retval_2 = runCkineU(self.ts, unkVec_IL2_IL2Raminus[:,ii])
             
             # dot yOut vectors by activity mask to generate total amount of active species 
-            
             actVec_IL2[ii] = np.dot(IL2_yOut[ii,:], self.activity)
             actVec_IL2_IL2Raminus[ii] = np.dot(IL2_yOut_IL2Raminus[ii,:], self.activity)
             
-        print('IL2 stimulation')
+        print('IL2 stimulation: IL2Ra+')
         print(actVec_IL2)
-        print(actVec_IL2_IL2Raminus) # TODO: figure out why this gets condensed to size of 1
+        print('IL2 stimulation: IL2Ra-')
+        print(actVec_IL2_IL2Raminus)
             
         # loop over concentrations of IL15
         unkVec_IL15 = np.zeros((24, 8))
@@ -203,7 +132,7 @@ class pstat:
         actVec_IL15 = np.zeros((8))
         actVec_IL15_IL2Raminus = actVec_IL15.copy()
         for ii in range(0,8):
-            unkVec_IL15[:, ii] = vec.copy()
+            unkVec_IL15[:, ii] = unkVec.copy()
             unkVec_IL15[0, ii] = self.cytokC[ii]
             
             unkVec_IL15_IL2Raminus[:,ii] = unkVec_IL15[:,ii].copy()
@@ -217,11 +146,14 @@ class pstat:
             actVec_IL15[ii] = np.dot(IL15_yOut[ii,:], self.activity)
             actVec_IL15_IL2Raminus[ii] = np.dot(IL15_yOut_IL2Raminus[ii,:], self.activity)
             
-        print('IL15 stimulation')
+        print('IL15 stimulation: IL2Ra+')
         print(actVec_IL15)
-        print(actVec_IL15_IL2Raminus) # TODO: figure out why this gets condensed to size of 1
+        print('IL15 stimulation: IL2Ra-')
+        print(actVec_IL15_IL2Raminus)
 
         # Normalize to the maximal activity, put together into one vector
         actVec = np.concatenate((actVec_IL2 / max(actVec_IL2), actVec_IL2_IL2Raminus / max(actVec_IL2_IL2Raminus), actVec_IL15 / max(actVec_IL15), actVec_IL15_IL2Raminus / max(actVec_IL15_IL2Raminus)))
         
         return actVec
+    
+# def plot(unkVec)
