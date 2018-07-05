9710903fb8a70973d2bcb7f9273b8e37aa85ea86
adamcweiner <adamcweiner@gmail.com>
2018-07-05 14:32:33 -0700

    add build_model and calc function

--- ckine/fit_others.py
+++ ckine/fit_others.py
@@ -27,19 +27,67 @@ class IL4_7_21_activity:
         
         Op = runCkineOp(ts=np.array(500.))
 
-        # Loop over concentrations of IL15
-        actVec = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVec[1], x))), self.cytokC))) # Change condensation here for activity
+        # Loop over concentrations of IL4
+        actVecIL4 = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVec[4], x))), self.cytokC)))
 
-        # Loop over concentrations of IL2
-        actVecIL2 = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVec[0], x))), self.cytokC)))
-
-        unkVecIL2RaMinus = T.set_subtensor(unkVec[18], 0.0) # Set IL2Ra to zero
-
-        # Loop over concentrations of IL2, IL2Ra-/-
-        actVecIL2RaMinus = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVecIL2RaMinus[0], x))), self.cytokC)))
+        # Loop over concentrations of IL7
+        actVecIL7 = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVec[2], x))), self.cytokC)))
+        
+        # Loop over concentrations of IL21
+        actVecIL21 = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVec[5], x))), self.cytokC)))
 
         # Normalize to the maximal activity, put together into one vector
-        actVec = T.concatenate((actVec / T.max(actVec), actVec / T.max(actVec), actVecIL2 / T.max(actVecIL2), actVecIL2RaMinus / T.max(actVecIL2RaMinus)))
+        actVec = T.concatenate((actVecIL4 / T.max(actVecIL4), actVecIL7 / T.max(actVecIL7), actVecIL21 / T.max(actVecIL21)))
 
-        # value we're trying to minimize is the distance between the y-values on points of the graph that correspond to the same IL2 values
+        # value we're trying to minimize is the distance between the y-values on points of the graph that correspond to the same lignad values and species
         return self.fit_data - actVec
+
+    
+class build_model:
+    """Going to load the data from the CSV file at the very beginning of when build_model is called... needs to be separate member function to avoid uploading file thousands of times."""
+    def __init__(self):
+        self.act = IL4_7_21_activity()
+        self.M = self.build()
+
+    def build(self):
+        """The PyMC model that incorporates Bayesian Statistics in order to store what the likelihood of the model is for a given point."""
+        M = pm.Model()
+
+        with M:
+            kfwd = pm.Lognormal('kfwd', mu=np.log(0.00001), sd=0.1)
+            rxnrates = pm.Lognormal('rxn', mu=np.log(0.1), sd=0.1, shape=3) # k27rev, k33rev, k35rev
+            endo_activeEndo = pm.Lognormal('endo', mu=np.log(0.1), sd=0.1, shape=2)
+            kRec_kDeg = pm.Lognormal('kRec_kDeg', mu=np.log(0.1), sd=0.1, shape=2)
+            GCexpr = pm.Lognormal('GCexpr', sd=0.1, shape=1) # expression of gc
+            IL7Raexpr = pm.Lognormal('IL7Raexpr', sd=0.1, shape=1) # expression of IL7Ra
+            Rexpr = pm.Lognormal('Rexpr', sd=0.1, shape=2) # Expression: IL4Ra, IL21Ra
+            sortF = pm.Beta('sortF', alpha=20, beta=40, testval=0.333)*0.95
+
+            ligands = T.zeros(6, dtype=np.float64)
+
+            unkVec = T.concatenate((ligands, T.stack(kfwd), rxnrates, endo_activeEndo, T.stack(sortF), kRec_kDeg, T.zeros(2, dtype=np.float64), T.stack(GCexpr), T.zeros(1, dtype=np.float64), T.stack(IL7Raexpr), T.zeros(1, dtype=np.float64), T.stack(Rexpr))) # receptor expression indexing same as in model.cpp
+            
+            Y_int = self.act.calc(unkVec) # fitting the data based on act.calc for the given parameters
+
+            pm.Deterministic('Y_int', T.sum(T.square(Y_int)))
+
+            pm.Normal('fitD_int', sd=T.std(Y_int), observed=Y_int)
+
+            # Save likelihood
+            pm.Deterministic('logp', M.logpt)
+
+        return M
+
+    def sampling(self):
+        """This is the sampling that actually runs the model."""
+        self.trace = pm.sample(init='advi', model=self.M)
+
+    def fit_ADVI(self):
+        with self.M:
+            approx = pm.fit(40000, method='fullrank_advi')
+            self.trace = approx.sample()
+
+    def profile(self):
+        """ Profile the gradient calculation. """
+        self.M.profile(pm.theanof.gradient(self.M.logpt, None)).summary()
+        
