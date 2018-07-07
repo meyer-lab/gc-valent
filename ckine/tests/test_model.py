80f5048f92d94495ddd6e422602b9076a8d3d262
adamcweiner <adamcweiner@gmail.com>
2018-07-07 08:42:23 -0700

    test_endosomal up and running

--- ckine/tests/test_model.py
+++ ckine/tests/test_model.py
@@ -81,19 +81,23 @@ class TestModel(unittest.TestCase):
         rxn[0:4] = 0. # set ligands to 0
         rxnIL2, rxnIL15, rxnIL7, rxnIL9 = rxn.copy(), rxn.copy(), rxn.copy(), rxn.copy()
         rxnIL2[0], rxnIL15[1], rxnIL7[2], rxnIL9[3] = 100., 100., 100., 100.
-        
+
         # runCkine to get yOut
         yOut_2, retVal = runCkineU(t, rxnIL2)
+        self.assertGreaterEqual(retVal, 0)
         yOut_15, retVal = runCkineU(t, rxnIL15)
+        self.assertGreaterEqual(retVal, 0)
         yOut_7, retVal = runCkineU(t, rxnIL7)
-        yOut_9, retVal = runCkineU(t, rxnIL7)
-        
+        self.assertGreaterEqual(retVal, 0)
+        yOut_9, retVal = runCkineU(t, rxnIL9)
+        self.assertGreaterEqual(retVal, 0)
+
         # check that dydt is ~0
-        self.assertPosEquilibrium(yOut_2[1], lambda y: fullModel(y, t, rxnIL2[0:13], rxnIL2[13:24]))
-        self.assertPosEquilibrium(yOut_15[1], lambda y: fullModel(y, t, rxnIL15[0:13], rxnIL15[13:24]))
-        self.assertPosEquilibrium(yOut_7[1], lambda y: fullModel(y, t, rxnIL7[0:13], rxnIL7[13:24]))
-        self.assertPosEquilibrium(yOut_9[1], lambda y: fullModel(y, t, rxnIL9[0:13], rxnIL9[13:24])) 
-        
+        self.assertPosEquilibrium(yOut_2[1], lambda y: fullModel(y, 100000.0, rxnIL2[0:13], rxnIL2[13:24]))
+        self.assertPosEquilibrium(yOut_15[1], lambda y: fullModel(y, 100000.0, rxnIL15[0:13], rxnIL15[13:24]))
+        self.assertPosEquilibrium(yOut_7[1], lambda y: fullModel(y, 100000.0, rxnIL7[0:13], rxnIL7[13:24]))
+        self.assertPosEquilibrium(yOut_9[1], lambda y: fullModel(y, 100000.0, rxnIL9[0:13], rxnIL9[13:24])) 
+
 
     def test_fullModel(self):
         """Assert the two functions solveAutocrine and solveAutocrine complete return the same values."""
