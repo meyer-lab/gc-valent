353e7af87f5f8dcd5c2ac624204bcbece3c60b12
adamcweiner <adamcweiner@gmail.com>
2018-07-11 14:23:50 -0700

    create size variable

--- ckine/figures/figure1.py
+++ ckine/figures/figure1.py
@@ -25,6 +25,7 @@ def IL2Rb_surf_perc(ax):
 
     y_max = 10.
     ts = np.array([0., 2., 5., 15., 30., 60., 90.])
+    size = len(ts)
     unkVec = np.array([0., 0., 0., 0., 2.02E-05, 0.38406199, 0.047815524, 0.118719663, 0.088470489, 0.092582828, 0.107673095, 0.101291622, 0.089522673, 0.023247533, 1.954609999, 0.042469514, 0.300662763, 0.004387724, 2.166630382, 0.673719144, 2.077778334, 0.288524177, 0., 0.])
 
     def plot_structure(IL2vec, IL15vec, title, ax):
@@ -38,14 +39,14 @@ def IL2Rb_surf_perc(ax):
 
 
     output = surf2Rb.calc(unkVec) * y_max
-    IL2_1_plus = output[0:7]
-    IL2_500_plus = output[7:14]
-    IL2_1_minus = output[14:21]
-    IL2_500_minus = output[21:28]
-    IL15_1_plus = output[28:35]
-    IL15_500_plus = output[35:42]
-    IL15_1_minus = output[42:49]
-    IL15_500_minus = output[49:56]
+    IL2_1_plus = output[0:(size)]
+    IL2_500_plus = output[(size):(size*2)]
+    IL2_1_minus = output[(size*2):(size*3)]
+    IL2_500_minus = output[(size*3):(size*4)]
+    IL15_1_plus = output[(size*4):(size*5)]
+    IL15_500_plus = output[(size*5):(size*6)]
+    IL15_1_minus = output[(size*6):(size*7)]
+    IL15_500_minus = output[(size*7):(size*8)]
 
     plot_structure(IL2_1_minus, IL15_1_minus, '1 nM and IL2Ra-', ax[0])
     plot_structure(IL2_500_minus, IL15_500_minus, "500 nM and IL2Ra-", ax[1])
