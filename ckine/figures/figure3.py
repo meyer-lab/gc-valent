fa05a74d1498d6a84dd60287ca9f77280d360368
Aaron Meyer <a@ameyer.me>
2018-07-11 15:47:15 -0700

    Block out cartoons.

--- ckine/figures/figure3.py
+++ ckine/figures/figure3.py
@@ -10,6 +10,9 @@ def makeFigure():
     # Get list of axis objects
     ax, f = getSetup((7, 6), (3, 4))
 
+    # Blank out for the cartoon
+    ax[0].axis('off')
+
     # Add subplot labels
     for ii, item in enumerate(ax):
         subplotLabel(item, string.ascii_uppercase[ii])
