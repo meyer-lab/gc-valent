4bd8a10bd5edb126b85a9088599e367005a13a97
adamcweiner <adamcweiner@gmail.com>
2018-07-11 14:11:20 -0700

    trying to generate subplots

--- ckine/figures/figure1.py
+++ ckine/figures/figure1.py
@@ -28,14 +28,14 @@ def IL2Rb_surf_perc(ax):
     unkVec = np.array([0., 0., 0., 0., 2.02E-05, 0.38406199, 0.047815524, 0.118719663, 0.088470489, 0.092582828, 0.107673095, 0.101291622, 0.089522673, 0.023247533, 1.954609999, 0.042469514, 0.300662763, 0.004387724, 2.166630382, 0.673719144, 2.077778334, 0.288524177, 0., 0.])
     
     def plot_structure(IL2vec, IL15vec, title, ax):
-        plt.title(title)
-        plt.scatter(ts, IL2vec, color='r', label='IL2', alpha=0.7, ax=ax)
-        plt.scatter(ts, IL15vec, color='g', label='IL15', alpha=0.7, ax=ax)
+        ax.title(title)
+        ax.scatter(ts, IL2vec, color='r', label='IL2', alpha=0.7)
+        ax.scatter(ts, IL15vec, color='g', label='IL15', alpha=0.7)
         # plt.ylim(0,(y_max + (0.2 * y_max)))
-        plt.ylabel("Surface IL2Rb (% x " + str(self.y_max) + ')')
-        plt.xlabel("Time (min)")
-        plt.legend()
-        plt.show()
+        ax.ylabel("Surface IL2Rb (% x " + str(self.y_max) + ')')
+        ax.xlabel("Time (min)")
+        ax.legend()
+        ax.show()
 
 
     output = surf2Rb.calc(unkVec) * y_max
