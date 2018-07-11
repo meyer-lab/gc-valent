37e332edaa3fe7c5e7bb59ee130e636998d932b4
adamcweiner <adamcweiner@gmail.com>
2018-07-11 13:51:56 -0700

    revert to original ax

--- ckine/figures/figure1.py
+++ ckine/figures/figure1.py
@@ -13,7 +13,7 @@ def makeFigure():
 
     subplotLabel(ax[0], 'A')
     
-    IL2Rb_surf_perc(ax[1:5])
+    IL2Rb_surf_perc(ax[0:4])
 
     f.tight_layout()
 
@@ -22,4 +22,4 @@ def makeFigure():
 def IL2Rb_surf_perc(ax):
     surf2Rb = surf_IL2Rb()
     unkVec = np.array([0., 0., 0., 0., 2.02E-05, 0.38406199, 0.047815524, 0.118719663, 0.088470489, 0.092582828, 0.107673095, 0.101291622, 0.089522673, 0.023247533, 1.954609999, 0.042469514, 0.300662763, 0.004387724, 2.166630382, 0.673719144, 2.077778334, 0.288524177, 0., 0.])
-    ax = surf2Rb.plot(unkVec)
+    ax[0:4] = surf2Rb.plot(unkVec)
