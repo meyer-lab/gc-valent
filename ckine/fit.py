Normalization fix. Fixes #165

ckine/fit.py
@@ -3,7 +3,6 @@ This file includes the classes and functions necessary to fit the IL2 model to t
 """
 import pymc3 as pm, theano.tensor as T, os
 from os.path import join
-from theano import shared
 import numpy as np, pandas as pds
 from .model import getTotalActiveSpecies, getSurfaceIL2RbSpecies
 from .differencing_op import runCkineOp, runCkineKineticOp
@@ -54,10 +53,9 @@ class IL2_15_activity:
         """This loads the experiment data and saves it as a member matrix and it also makes a vector of the IL15 concentrations that we are going to take care of."""
         path = os.path.dirname(os.path.abspath(__file__))
         data = pds.read_csv(join(path, "./data/IL2_IL15_extracted_data.csv")).values # imports csv file into pandas array
-        dataIL2 = pds.read_csv(join(path, "./data/IL2_IL15_extracted_data.csv")).values # imports csv file into pandas array
         self.cytokC = np.logspace(-3.3, 2.7, 8) # 8 log-spaced values between our two endpoints
 
-        self.fit_data = np.concatenate((data[:, 7], data[:, 3], dataIL2[:, 6], dataIL2[:, 2])) / 100. #the IL15_IL2Ra- data is within the 4th column (index 3)
+        self.fit_data = np.concatenate((data[:, 7], data[:, 3], data[:, 6], data[:, 2])) / 100. #the IL15_IL2Ra- data is within the 4th column (index 3)
 
         self.activity = getTotalActiveSpecies().astype(np.float64)
 
@@ -81,10 +79,10 @@ class IL2_15_activity:
         actVecIL2RaMinus = T.stack(list(map(lambda x: T.dot(self.activity, Op(T.set_subtensor(unkVecIL2RaMinus[0], x))), self.cytokC)))
 
         # Normalize to the maximal activity, put together into one vector
-        actVec = T.concatenate((actVec / T.max(actVec), actVec / T.max(actVec), actVecIL2 / T.max(actVecIL2), actVecIL2RaMinus / T.max(actVecIL2RaMinus)))
+        actCat = T.concatenate((actVec, actVec, actVecIL2, actVecIL2RaMinus))
 
         # value we're trying to minimize is the distance between the y-values on points of the graph that correspond to the same IL2 values
-        return self.fit_data - actVec
+        return self.fit_data - (actCat / T.max(actCat))
 
 
 class build_model: