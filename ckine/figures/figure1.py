b3ca9e386c1abbbb3651141f8ca75fdfd18c0596
adamcweiner <adamcweiner@gmail.com>
2018-07-11 12:50:12 -0700

    first attempt at IL2Rb in figure1

--- ckine/figures/figure1.py
+++ ckine/figures/figure1.py
@@ -2,6 +2,7 @@
 This creates Figure 1.
 """
 from .figureCommon import subplotLabel, getSetup
+from ..plot_model_prediction import surf_IL2Rb, pstat, surf_gc
 
 
 def makeFigure():
@@ -10,7 +11,14 @@ def makeFigure():
     ax, f = getSetup((7, 6), (3, 4))
 
     subplotLabel(ax[0], 'A')
+    
+    IL2Rb_surf_perc(ax[0:4])
 
     f.tight_layout()
 
     return f
+
+def IL2Rb_surf_perc(ax):
+    surf2Rb = surf_IL2Rb()
+    unkVec = np.array([0., 0., 0., 0., 2.02E-05, 0.38406199, 0.047815524, 0.118719663, 0.088470489, 0.092582828, 0.107673095, 0.101291622, 0.089522673, 0.023247533, 1.954609999, 0.042469514, 0.300662763, 0.004387724, 2.166630382, 0.673719144, 2.077778334, 0.288524177, 0., 0.])
+    ax[0:4] = surf2Rb.plot(unkVec)
