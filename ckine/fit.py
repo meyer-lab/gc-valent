2c4ceb9156846d56b8ab5f2cf736c559576483fb
adamcweiner <adamcweiner@gmail.com>
2018-07-15 22:03:46 -0700

    loosen prior stdev for reverse rxn rates

--- ckine/fit.py
+++ ckine/fit.py
@@ -100,7 +100,7 @@ class build_model:
 
         with M:
             kfwd = pm.Lognormal('kfwd', mu=np.log(0.00001), sd=0.1)
-            rxnrates = pm.Lognormal('rxn', mu=np.log(0.1), sd=0.1, shape=10) # there are 10 reverse rxn rates
+            rxnrates = pm.Lognormal('rxn', mu=np.log(0.1), sd=10, shape=10) # there are 10 reverse rxn rates
             endo_activeEndo = pm.Lognormal('endo', mu=np.log(0.1), sd=0.1, shape=2)
             kRec_kDeg = pm.Lognormal('kRec_kDeg', mu=np.log(0.1), sd=0.1, shape=2)
             Rexpr = pm.Lognormal('IL2Raexpr', sd=0.1, shape=4) # Expression: IL2Ra, IL2Rb, gc, IL15Ra
