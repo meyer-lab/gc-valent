7d3f1445bef80a269955ce24735e30391c444446
adamcweiner <adamcweiner@gmail.com>
2018-07-17 15:33:51 -0700

    update priors

--- ckine/fit_others.py
+++ ckine/fit_others.py
@@ -52,12 +52,12 @@ class build_model:
         M = pm.Model()
 
         with M:
-            kfwd = pm.Lognormal('kfwd', mu=np.log(0.00001), sd=0.1, shape=1)
+            kfwd = pm.Lognormal('kfwd', mu=np.log(0.00001), sd=10, shape=1)
             nullRates = T.ones(6, dtype=np.float64) # associated with IL2 and IL15
             Tone = T.ones(1, dtype=np.float64)
             Tzero = T.zeros(1, dtype=np.float64)
-            k27rev = pm.Lognormal('k27rev', mu=np.log(0.1), sd=0.1, shape=1) # associated with IL7
-            k33rev = pm.Lognormal('k33rev', mu=np.log(0.1), sd=0.1, shape=1) # associated with IL4
+            k27rev = pm.Lognormal('k27rev', mu=np.log(0.1), sd=10, shape=1) # associated with IL7
+            k33rev = pm.Lognormal('k33rev', mu=np.log(0.1), sd=10, shape=1) # associated with IL4
             endo_activeEndo = pm.Lognormal('endo', mu=np.log(0.1), sd=0.1, shape=2)
             kRec_kDeg = pm.Lognormal('kRec_kDeg', mu=np.log(0.1), sd=0.1, shape=2)
             GCexpr = (328. * endo_activeEndo[0]) / (1 + (kRec_kDeg[0] / kRec_kDeg[1])) # constant according to measured number per cell
@@ -65,7 +65,7 @@ class build_model:
             IL4Raexpr = (254. * endo_activeEndo[0]) / (1 + (kRec_kDeg[0] / kRec_kDeg[1])) # constant according to measured number per cell
             sortF = pm.Beta('sortF', alpha=20, beta=40, testval=0.333, shape=1)*0.95
             # TODO: double check the priors for scales seem reasonable
-            scales = pm.Lognormal('scales', mu=np.log(10), sd=np.log(25), shape=2) # create scaling constants for activity measurements
+            scales = pm.Lognormal('scales', mu=np.log(100), sd=np.log(25), shape=2) # create scaling constants for activity measurements
 
             ligands = T.zeros(6, dtype=np.float64)
             
