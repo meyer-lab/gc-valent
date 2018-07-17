d324f055b7d945fa2674e69fc9d38d9a7ee7e392
adamcweiner <adamcweiner@gmail.com>
2018-07-17 13:32:52 -0700

    add import samples function

--- ckine/figures/figure2.py
+++ ckine/figures/figure2.py
@@ -1,7 +1,16 @@
 """
 This creates Figure 2.
 """
-from .figureCommon import subplotLabel, getSetup
+from .figureCommon import subplotLabel, getSetup, rev_rxn_names, traf_names, Rexpr_names
+from ..plot_model_prediction import pstat
+from ..model import nParams
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import pymc3 as pm, os
+from os.path import join
+from ..fit import build_model
 
 
 def makeFigure():
@@ -17,3 +26,29 @@ def makeFigure():
     f.tight_layout()
 
     return f
+
+def import_samples():
+    bmodel = build_model()
+    n_params = nParams()
+
+    path = os.path.dirname(os.path.abspath(__file__))
+    trace = pm.backends.text.load(join(path, '../../IL4-7_model_results'), bmodel.M)
+    kfwd = trace.get_values('kfwd', chains=[0])
+    k27rev = trace.get_values('k27rev', chains=[0])
+    k33rev = trace.get_values('k33rev', chains=[0])
+    endo_activeEndo = trace.get_values('endo', chains=[0])
+    sortF = trace.get_values('sortF', chains=[0])
+    kRec_kDeg = trace.get_values('kRec_kDeg', chains=[0])
+    scales = trace.get_values('scales', chains=[0])
+    
+    GCexpr = (328. * endo_activeEndo[:, 0]) / (1 + (kRec_kDeg[:, 0] / kRec_kDeg[:, 1]))
+    IL7Raexpr = (2591. * endo_activeEndo[:, 0]) / (1 + (kRec_kDeg[:, 0] / kRec_kDeg[:, 1]))
+    IL4Raexpr = (254. * endo_activeEndo[:, 0]) / (1 + (kRec_kDeg[:, 0] / kRec_kDeg[:, 1]))
+    
+    
+    
+    unkVec = np.zeros((n_params, 500))
+    for ii in range (0, 500):
+        unkVec[:, ii] = np.array([0., 0., 0., 0., 0., 0., kfwd[ii], 0., 0., 0., 0., 0., 0., k27rev[ii], 0., k33rev[ii], 0., endo_activeEndo[ii, 0], endo_activeEndo[ii, 1], sortF[ii], kRec_kDeg[ii, 0], kRec_kDeg[ii, 1], 0., 0., GCexpr[ii], 0., IL7Raexpr[ii], 0., IL4Raexpr[ii], 0.])
+    
+    return unkVec
