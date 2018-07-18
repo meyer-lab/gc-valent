6b3c736909a3a5673a197d8c3153f2edd99bac56
adamcweiner <adamcweiner@gmail.com>
2018-07-18 12:37:13 -0700

    condense plot_structure into 1 function

--- ckine/figures/figure1.py
+++ ckine/figures/figure1.py
@@ -30,6 +30,23 @@ def makeFigure():
 
     return f
 
+def plot_structure(IL2vec, IL15vec, title, ax, x_axis, y_max, data_type, species='NONE'):
+    ax.set_title(title)
+    ax.set_ylim(0,(y_max + (0.2 * y_max)))
+    if (data_type=='surf'):
+        ax.scatter(x_axis, IL2vec, color='r', label='IL2', alpha=0.7)
+        ax.scatter(x_axis, IL15vec, color='g', label='IL15', alpha=0.7)
+        ax.set_ylabel("Surface " + str(species) + " (% x " + str(y_max) + ')')
+        ax.set_xlabel("Time (min)")
+    elif (data_type=='act'):
+        ax.scatter(np.log10(x_axis), IL2vec, color='r', alpha=0.5, label="IL2")
+        ax.scatter(np.log10(x_axis), IL15vec, color='g', alpha=0.5, label='IL15')
+        ax.set_ylabel('Maximal p-STAT5 (% x ' + str(y_max) + ')')
+        ax.set_xlabel('log10 of cytokine concentration (nM)')
+    else:
+        print('invalid data_type')   
+    # ax.legend()
+
 def surf_perc(ax, species):
     if (species == 'IL2Rb'):
         surf = surf_IL2Rb()
@@ -44,15 +61,6 @@ def surf_perc(ax, species):
     size = len(ts)
     unkVec = import_samples()
 
-    def plot_structure(IL2vec, IL15vec, title, ax):
-        ax.set_title(title)
-        ax.scatter(ts, IL2vec, color='r', label='IL2', alpha=0.7)
-        ax.scatter(ts, IL15vec, color='g', label='IL15', alpha=0.7)
-        ax.set_ylim(0,(y_max + (0.2 * y_max)))
-        ax.set_ylabel("Surface " + str(species) + " (% x " + str(y_max) + ')')
-        ax.set_xlabel("Time (min)")
-        # ax.legend()
-
     for ii in range(0,500):
         output = surf.calc(unkVec[:, ii], ts) * y_max
         IL2_1_plus = output[0:(size)]
@@ -64,10 +72,10 @@ def surf_perc(ax, species):
         IL15_1_minus = output[(size*6):(size*7)]
         IL15_500_minus = output[(size*7):(size*8)]
 
-        plot_structure(IL2_1_minus, IL15_1_minus, '1 nM and IL2Ra-', ax[0])
-        plot_structure(IL2_500_minus, IL15_500_minus, "500 nM and IL2Ra-", ax[1])
-        plot_structure(IL2_1_plus, IL15_1_plus, "1 nM and IL2Ra+", ax[2])
-        plot_structure(IL2_500_plus, IL15_500_plus, "500 nM and IL2Ra+", ax[3])
+        plot_structure(IL2_1_minus, IL15_1_minus, '1 nM and IL2Ra-', ax[0], ts, y_max, 'surf', species)
+        plot_structure(IL2_500_minus, IL15_500_minus, "500 nM and IL2Ra-", ax[1], ts, y_max, 'surf', species)
+        plot_structure(IL2_1_plus, IL15_1_plus, "1 nM and IL2Ra+", ax[2], ts, y_max, 'surf', species)
+        plot_structure(IL2_500_plus, IL15_500_plus, "500 nM and IL2Ra+", ax[3], ts, y_max, 'surf', species)
 
     
 def pstat_act(ax):
@@ -76,15 +84,6 @@ def pstat_act(ax):
     cytokC = np.logspace(-3.3, 2.7, PTS)
     y_max = 100.
     unkVec = import_samples()
-    
-    def plot_structure(IL2vec, IL15vec, title, ax):
-        ax.set_title(title)
-        ax.scatter(np.log10(cytokC), IL2vec, color='r', alpha=0.5, label="IL2")
-        ax.scatter(np.log10(cytokC), IL15vec, color='g', alpha=0.5, label='IL15')
-        ax.set_ylim(0,(y_max + (0.25*y_max)))
-        ax.set_ylabel('Maximal p-STAT5 (% x ' + str(y_max) + ')')
-        ax.set_xlabel('log10 of cytokine concentration (nM)')
-        # ax.legend()
 
     for ii in range(0,500):
         output = pstat5.calc(unkVec[:, ii], cytokC) * y_max
@@ -93,8 +92,8 @@ def pstat_act(ax):
         IL15_plus = output[(PTS*2):(PTS*3)]
         IL15_minus = output[(PTS*3):(PTS*4)]
 
-        plot_structure(IL2_minus, IL15_minus, "IL2Ra- YT-1 cells", ax[0])
-        plot_structure(IL2_plus, IL15_plus, "IL2Ra+ YT-1 cells", ax[1])
+        plot_structure(IL2_minus, IL15_minus, "IL2Ra- YT-1 cells", ax[0], cytokC, y_max, 'act')
+        plot_structure(IL2_plus, IL15_plus, "IL2Ra+ YT-1 cells", ax[1], cytokC, y_max, 'act')
 
     
 def import_samples():
