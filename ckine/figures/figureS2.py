565df8ccb39bc333dd6f3ebfced6c43ae7bd12f8
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2018-07-18 09:53:32 -0700

    Update figureS2.py

--- ckine/figures/figureS2.py
+++ ckine/figures/figureS2.py
@@ -12,11 +12,11 @@ from ..Tensor_analysis import perform_decomposition
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    x, y = 3, 5
+    x, y = 5, 5
     ax, f = getSetup((10, 10), (x, y))
 
     values, _, _, _, cell_names = prepare_tensor(2)
-    factors = perform_decomposition(values, 6)
+    factors = perform_decomposition(values, 2*x)
 
     for row in range(x):
         compNum = 2*row + 1
