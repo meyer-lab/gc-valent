97b25bc9bd816fe82ab4cf424cd70a4bfebd6b97
Ali <alif@asmlab.org>
2018-07-31 21:48:08 -0700

    Now for all quantities

--- ckine/figures/figure3.py
+++ ckine/figures/figure3.py
@@ -61,14 +61,17 @@ def plot_R2X(ax, tensor, factors_list, n_comps):
 
 def plot_R2X_singles(ax, values, factors_list, n_comps):
     """R2X plot for removing single components from final factorization."""
-    R2X_array = R2X_singles(values, factors_list, n_comps)
-    ax.bar(range(1, n_comps+1), R2X_array)
+    R2X_singles_matrix = R2X_singles(values, factors_list, n_comps)
+    ax.plot(range(1,n_comps+1), R2X_singles_matrix[0,:], 'ko', label = 'Overall R2X')
+    ax.plot(range(1,n_comps+1), R2X_singles_matrix[1,:], 'bo', label = 'Ligand Activity R2X')
+    ax.plot(range(1,n_comps+1), R2X_singles_matrix[2,:], 'ro', label = 'Surface Receptors R2X')
+    ax.plot(range(1,n_comps+1), R2X_singles_matrix[3,:], 'go', label = 'Total Receptors R2X')    
     ax.set_ylabel('R2X')
     ax.set_xlabel('Component Index')
     ax.set_ylim(0, 1)
     ax.set_xticks(np.arange(1, n_comps+1, 2))
     ax.set_xticklabels(np.arange(1, n_comps+1, 2))
-    
+    ax.legend()
 
 def plot_split_R2X(ax, tensor, factors_list, n_comps):
     """This function takes in the values tensor, splits it up into a mini tensor corresponding to the splitType. If splitType =1, then split ligand activities, if =2, then split surface receptors, if = 3, then split total receptors."""
