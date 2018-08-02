b4a374b4e553cc8290fa48c830fcd98f6f7f7fc5
adamcweiner <adamcweiner@gmail.com>
2018-08-02 12:18:21 -0700

    clean up fit.py

--- ckine/fit.py
+++ ckine/fit.py
@@ -10,16 +10,16 @@ from .model import getTotalActiveSpecies, getSurfaceIL2RbSpecies
 from .differencing_op import runCkineOp, runCkineKineticOp, runCkineDoseOp
 
 
-def load_data(filename): 
+def load_data(filename):
+    """ Return path of CSV files. """
     path = dirname(abspath(__file__))
     return pds.read_csv(join(path, filename)).values
 
 
 class IL2Rb_trafficking:
+    """ Calculating the percent of IL2Rb on cell surface under IL2 and IL15 stimulation according to Ring et al."""
     def __init__(self):
-        # all of the IL2Rb trafficking data with IL2Ra+... first row contains headers... 9 columns and 8 rows... first column is time
         numpy_data = load_data('data/IL2Ra+_surface_IL2RB_datasets.csv')
-        # all of the IL2Rb trafficking data with IL2Ra-... first row contains headers... 9 columns and 8 rows... first column is time
         numpy_data2 = load_data('data/IL2Ra-_surface_IL2RB_datasets.csv')
 
         # times from experiment are hard-coded into this function
@@ -31,6 +31,7 @@ class IL2Rb_trafficking:
         self.data = np.concatenate((numpy_data[:, slicingg], numpy_data2[:, slicingg])).flatten(order='F')/10.
 
     def calc(self, unkVec):
+        """ Calculates difference between relative IL2Rb on surface in model prediction and Ring experiment. """
         unkVecIL2RaMinus = T.set_subtensor(unkVec[22], 0.0) # Set IL2Ra to zero
 
         # Condense to just IL2Rb
@@ -47,16 +48,13 @@ class IL2Rb_trafficking:
         g = KineticOp(T.set_subtensor(unkVecIL2RaMinus[1], 1.))
         h = KineticOp(T.set_subtensor(unkVecIL2RaMinus[1], 500.))
 
-        # assuming all IL2Rb starts on the cell surface
+        # return residual assuming all IL2Rb starts on the cell surface
         return T.concatenate((a, b, e, f, c, d, g, h)) / a[0] - self.data
 
-# this takes all the desired IL2 values we want to test and gives us the maximum activity value
-# IL2 values pretty much ranged from 5 x 10**-4 to 500 nm with 8 points in between
-# need the theano decorator to get around the fact that there are if-else statements when running odeint but
-# we don't necessarily know the values for the rxn rates when we call our model
+
 class IL2_15_activity:
+    """ Calculating the pSTAT activity residuals for IL2 and IL15 stimulation in Ring et al. """
     def __init__(self):
-        """ This loads the experiment data and saves it as a member matrix and it also makes a vector of the IL15 concentrations that we are going to take care of. """
         path = dirname(abspath(__file__))
         data = load_data('./data/IL2_IL15_extracted_data.csv')
         self.cytokC = np.logspace(-3.3, 2.7, 8) # 8 log-spaced values between our two endpoints
@@ -68,7 +66,7 @@ class IL2_15_activity:
         self.fit_data = np.concatenate((data[:, 6], data[:, 7], data[:, 2], data[:, 3])) / 100. #the IL15_IL2Ra- data is within the 4th column (index 3)
 
     def calc(self, unkVec):
-        """ Simulate the STAT5 measurements. """
+        """ Simulate the STAT5 measurements and return residuals between model prediction and experimental data. """
         # We don't need the ligands in unkVec for this Op
         unkVec = unkVec[6::]
 
@@ -80,14 +78,12 @@ class IL2_15_activity:
         # Normalize to the maximal activity, put together into one vector
         actCat = T.concatenate((Op(unkVec), Op(unkVecIL2RaMinus)))
 
-        # value we're trying to minimize is the distance between the y-values on points of the graph that correspond to the same IL2 values
+        # return the residual
         return self.fit_data - (actCat / T.max(actCat))
 
 
 class build_model:
     """ Build the overall model handling Ring et al. """
-    # Going to load the data from the CSV file at the very beginning of when build_model is called...
-    # needs to be separate member function to avoid uploading file thousands of times.
     def __init__(self):
         self.dst15 = IL2_15_activity()
         self.IL2Rb = IL2Rb_trafficking()
@@ -116,7 +112,7 @@ class build_model:
             pm.Deterministic('Y_15', T.sum(T.square(Y_15)))
             pm.Deterministic('Y_int', T.sum(T.square(Y_int)))
 
-            pm.Normal('fitD_15', sd=0.05, observed=Y_15) # TODO: Replace with experimental-derived stderr
+            pm.Normal('fitD_15', sd=0.05, observed=Y_15) # experimental-derived stderr is used
             pm.Normal('fitD_int', sd=0.02, observed=Y_int)
 
             # Save likelihood
