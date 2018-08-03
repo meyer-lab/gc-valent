12f712304138f64d895be232d5ad7013875d3cad
adamcweiner <adamcweiner@gmail.com>
2018-08-03 15:22:40 -0700

    went from 500 unkVec's to 1

--- ckine/figures/figure4.py
+++ ckine/figures/figure4.py
@@ -21,7 +21,7 @@ def makeFigure():
     data, cell_names = load_cells()
     unkVec = import_samples_2_15()
     
-    all_cells(ax[0], data, cell_names, unkVec)
+    all_cells(ax[0], data, cell_names, unkVec[:, 0])
 
     f.tight_layout()
 
@@ -30,11 +30,9 @@ def makeFigure():
 def single_cell_act(unkVec, cytokC):
     """ Cytokine activity for all IL2 doses for single cell line. """
     pstat5 = pstat()
-    act = np.zeros((cytokC.shape[0], unkVec.shape[1]))
-    for ii in range(unkVec.shape[1]):
-        act[:, ii] = np.fromiter((pstat5.singleCalc(unkVec[:, ii], 0, x) for x in cytokC), np.float64)
-    act_mean = np.mean(act, axis=1)
-    return act_mean
+    act = np.zeros((cytokC.shape[0]))
+    act = np.fromiter((pstat5.singleCalc(unkVec, 0, x) for x in cytokC), np.float64)
+    return act
     
     
 def all_cells(ax, cell_data, cell_names, unkVec):
@@ -45,12 +43,10 @@ def all_cells(ax, cell_data, cell_names, unkVec):
     numCells = cell_data.shape[1] - 1   # first column is receptor names
     results = np.zeros((PTS, numCells))
     
-    # for ii in range(1, numCells+1):       # for all cell types
-    for ii in range(1, 3):      # just testing first two cell types for now
-        for n in range(unkVec.shape[1]):
-            unkVec[22:30, n] = cell_data[:, ii] # place cell data in all rows of unkVec
-        results[:, ii] = single_cell_act(unkVec, cytokC)
-        ax.plot(np.log10(cytokC), results[:, ii], label=cell_names[ii-1])
+    # for ii in range(0, numCells):       # for all cell types
+    for ii in range(0, 2):      # just testing first two cell types for now
+        unkVec[22:30] = cell_data[:, ii+1] # place cell data in all rows of unkVec
+        ax.plot(np.log10(cytokC), single_cell_act(unkVec, cytokC), label=cell_names[ii])
         print("done with " + str(ii) + " cell type")
         
     ax.legend()
