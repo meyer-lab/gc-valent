2f8068b91cc6ade936e3fa48f7501c3f0527977f
adamcweiner <adamcweiner@gmail.com>
2018-08-03 15:03:15 -0700

    working for the first two cell types

--- ckine/figures/figure4.py
+++ ckine/figures/figure4.py
@@ -27,12 +27,10 @@ def makeFigure():
 
     return f
 
-def single_cell_act(unkVec):
+def single_cell_act(unkVec, cytokC):
     """ Cytokine activity for all IL2 doses for single cell line. """
     pstat5 = pstat()
-    PTS = 30
-    cytokC = np.logspace(-3.3, 2.7, PTS)
-    act = np.zeros((PTS, unkVec.shape[1]))
+    act = np.zeros((cytokC.shape[0], unkVec.shape[1]))
     for ii in range(unkVec.shape[1]):
         act[:, ii] = np.fromiter((pstat5.singleCalc(unkVec[:, ii], 0, x) for x in cytokC), np.float64)
     act_mean = np.mean(act, axis=1)
@@ -42,9 +40,20 @@ def single_cell_act(unkVec):
 def all_cells(ax, cell_data, cell_names, unkVec):
     """ Loops through all cell types and calculates activities. """
     cell_data = cell_data.values    # convert to numpy array
-    for ii in range(1, cell_data.shape[1]):
+    PTS = 30    # number of cytokine concentrations that are used
+    cytokC = np.logspace(-3.3, 2.7, PTS)
+    numCells = cell_data.shape[1] - 1   # first column is receptor names
+    results = np.zeros((PTS, numCells))
+    
+    # for ii in range(1, numCells+1):       # for all cell types
+    for ii in range(1, 3):      # just testing first two cell types for now
         for n in range(unkVec.shape[1]):
             unkVec[22:30, n] = cell_data[:, ii] # place cell data in all rows of unkVec
-        print(single_cell_act(unkVec))
+        results[:, ii] = single_cell_act(unkVec, cytokC)
+        ax.plot(np.log10(cytokC), results[:, ii], label=cell_names[ii-1])
+        print("done with " + str(ii) + " cell type")
         
+    ax.legend()
+    # plot: x-axis is cytokC, y_axis is activity, different lines are cell types
+    #ax.plot(cytokC, results[:, 0], title=cell_names[0])    
 
