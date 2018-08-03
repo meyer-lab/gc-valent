3fadb1715b470f04be212128eb391b02fd226330
adamcweiner <adamcweiner@gmail.com>
2018-08-03 11:54:13 -0700

    calc mean activity for all 500 samples in unkVec

--- ckine/figures/figure4.py
+++ ckine/figures/figure4.py
@@ -20,6 +20,8 @@ def makeFigure():
 
     data, cell_names = load_cells()
     unkVec = import_samples_2_15()
+    
+    IL2_vs_cells(ax[0], data, cell_names, unkVec)
 
     f.tight_layout()
 
@@ -27,7 +29,14 @@ def makeFigure():
 
 def IL2_vs_cells(ax, cell_data, cell_names, unkVec):
     """ Cytokine activity vs IL2 dose for all cell populations. """
+    pstat5 = pstat()
     PTS = 30
     cytokC = np.logspace(-3.3, 2.7, PTS)
-    act = np.fromiter((pstat.singleCalc(unkVec, 0, x) for x in cytokC), np.float64)
+    print("unkVec.shape: " + str(unkVec.shape))
+    act = np.zeros((PTS, unkVec.shape[1]))
+    for ii in range(unkVec.shape[1]):
+        act[:, ii] = np.fromiter((pstat5.singleCalc(unkVec[:, ii], 0, x) for x in cytokC), np.float64)
+    print("act.shape: " + str(act.shape))
+    act_mean = np.mean(act, axis=1)
+    print("act_mean.shape: " + str(act_mean.shape))
 
