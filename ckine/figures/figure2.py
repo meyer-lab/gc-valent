f19c7b846abf504846265c3b9aa05d299b1d9b87
adamcweiner <adamcweiner@gmail.com>
2018-10-10 16:38:26 -0700

    changes for poster rough draft

--- ckine/figures/figure2.py
+++ ckine/figures/figure2.py
@@ -134,14 +134,20 @@ def violinPlots(ax, unkVec, scales):
     rxn.columns = ['kfwd', 'k27rev', 'k33rev']
     a = sns.violinplot(data=np.log10(rxn), ax=ax[0])  # creates names based on dataframe columns
     a.set_xticklabels(a.get_xticklabels(), rotation=40, rotation_mode="anchor", ha="right", fontsize=8, position=(0,0.045))
+    a.set_ylabel("log10 of value")
+    a.set_title("Reverse reaction rates")
 
     traf.columns = traf_names()
     b = sns.violinplot(data=traf, ax=ax[1])
     b.set_xticklabels(b.get_xticklabels(), rotation=40, rotation_mode="anchor", ha="right", fontsize=8, position=(0,0.045))
+    b.set_ylabel("value")
+    b.set_title("Trafficking parameters")
 
     Rexpr.columns = ['GCexpr', 'IL7Raexpr', 'IL4Raexpr']
     c = sns.violinplot(data=Rexpr, ax=ax[2])
     c.set_xticklabels(c.get_xticklabels(), rotation=40, rotation_mode="anchor", ha="right", fontsize=8, position=(0,0.045))
+    c.set_ylabel("value")
+    c.set_title("Receptor expression rates")
 
     scales.columns = ['IL4 scale', 'IL7 scale']
     d = sns.violinplot(data=scales, ax=ax[3])
