f37b8eba8d468dca173122b89d40aed838b2f8fb
Ali <alif@asmlab.org>
2018-11-07 02:52:00 -0800

    linting cupy

--- ckine/figures/figureS2.py
+++ ckine/figures/figureS2.py
@@ -39,7 +39,7 @@ def makeFigure():
     out = perform_tucker(values, rank_list)
 
     factors = out[1]
-    plot_timepoints(ax[0], factors[0])
+    plot_timepoints(ax[0], cp.asnumpy(factors[0]))
 
     for row in range(x):
         subplotLabel(ax[row], string.ascii_uppercase[row]) # Add subplot labels
@@ -53,13 +53,13 @@ def makeFigure():
         if row > np.floor(rank_list[3]/2):
             ax[row*y +3].axis('off')
         
-        plot_cells(ax[row*y + 1], factors[1], compNum, compNum+1, cell_names, ax_pos = row*y + 1)
+        plot_cells(ax[row*y + 1], cp.asnumpy(factors[1]), compNum, compNum+1, cell_names, ax_pos = row*y + 1)
         if compNum < rank_list[2]:
-            plot_ligands(ax[row*y + 2], factors[2], compNum, compNum+1)
+            plot_ligands(ax[row*y + 2], cp.asnumpy(factors[2]), compNum, compNum+1)
         if compNum < rank_list[3]:
-            plot_values(ax[row*y + 3] , factors[3], compNum, compNum+1, ax_pos = row*y + 3)
+            plot_values(ax[row*y + 3] , cp.asnumpy(factors[3]), compNum, compNum+1, ax_pos = row*y + 3)
         elif compNum == rank_list[3]:
-            plot_values(ax[row*y + 3] , factors[3], compNum-1, compNum, ax_pos = row*y + 3)
+            plot_values(ax[row*y + 3] , cp.asnumpy(factors[3]), compNum-1, compNum, ax_pos = row*y + 3)
 
         # Set axes to center on the origin, and add labels
         for col in range(1,y):
@@ -75,6 +75,7 @@ def makeFigure():
 
             ax[row*y + col].set_xlim(-x_max, x_max)
             ax[row*y + col].set_ylim(-y_max, y_max)
-
+    print('Finding R2X')
+    print('Finding R2X')
     print(find_R2X_tucker(values, out, subt = True))
     return f
