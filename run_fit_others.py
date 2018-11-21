aa8512f703b57f162bdc1924d3b9ae7ac01a543d
adamcweiner <adamcweiner@gmail.com>
2018-11-20 22:29:27 -0800

    works most of the time but rev rxn rates are sometimes inf

--- run_fit_others.py
+++ run_fit_others.py
@@ -2,6 +2,8 @@
 
 # Set matplotlib backend so python remains in the background
 import matplotlib
+import numpy as np
+import random
 matplotlib.use("Agg")
 from ckine.fit_others import build_model
 import pymc3 as pm
@@ -9,9 +11,10 @@ import pymc3 as pm
 if __name__ == "__main__": #only go into this loop if you're running fit.py directly instead of running a file that calls fit.py
     M = build_model(pretreat=True)
     M.build()
-    ex = np.linspace(10**-8, 10**4)
+    ex = np.linspace(10**-8, 10**3)
     # randomly select arguments for sampling from ex
-    M.sampling()
-    print(M.trace)
+    for ii in range(25):
+        M.sampling(random.choice(ex), random.choice(ex), random.choice(ex), random.choice(ex))
+        print(M.trace)
     # pm.backends.text.dump("IL4-7_model_results", M.trace) #instead of pickling data we dump it into file that can be accessed by read_fit_data.py
     # pm.backends.text.Text("IL4-7_find_MAP_results", model=M)
