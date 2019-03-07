fix log scaling and fig4

ckine/figures/figure2.py
@@ -119,19 +119,20 @@ def violinPlots(ax, unkVec, scales):
     a.set_title("Reverse reaction rates")
 
     traf.columns = traf_names()
-    b = sns.violinplot(data=traf, ax=ax[1], linewidth=0.5)
+    b = sns.violinplot(data=np.log10(traf), ax=ax[1], linewidth=0.5)
     b.set_xticklabels(b.get_xticklabels(), rotation=40, rotation_mode="anchor", ha="right", fontsize=8, position=(0,0.045))
     b.set_ylabel(r"$\mathrm{log_{10}(\frac{1}{min})}$")
     b.set_title("Trafficking parameters")
 
     Rexpr.columns = [r'$\gamma_{c}$', 'IL-7Rα', 'IL-4Rα']
-    c = sns.violinplot(data=Rexpr, ax=ax[2], linewidth=0.5)
+    c = sns.violinplot(data=np.log10(Rexpr), ax=ax[2], linewidth=0.5)
     c.set_xticklabels(c.get_xticklabels(), rotation=40, rotation_mode="anchor", ha="right", fontsize=8, position=(0,0.045))
     c.set_ylabel(r"$\mathrm{log_{10}(\frac{num}{cell * min})}$")
     c.set_title("Receptor expression rates")
 
     scales.columns = [r'$C_{6}$', r'$C_{5}$']
     d = sns.violinplot(data=scales, ax=ax[3], linewidth=0.5)
+    d.set_ylabel("value")
     d.set_title("pSTAT scaling constants")
 
 def pretreat_calc(unkVec, scales, pre_conc):

ckine/figures/figure4.py
@@ -46,7 +46,7 @@ def relativeGC(ax, unkVec2, unkVec4):
     col_list_palette = sns.xkcd_palette(col_list)
     cmap = sns.set_palette(col_list_palette)
 
-    a = sns.violinplot(data=np.log10(df), ax=ax, linewidth=0, bw=10, cmap=cmap, scale='width')
+    a = sns.violinplot(data=np.log10(df), ax=ax, linewidth=0, cmap=cmap, scale='width')
     a.set_xticklabels(a.get_xticklabels(), rotation=40, rotation_mode="anchor", ha="right", fontsize=8, position=(0, 0.075))
     a.set(title=r"Relative $\gamma_{c}$ affinity", ylabel=r"$\mathrm{log_{10}(\frac{1}{nM * min})}$")
 