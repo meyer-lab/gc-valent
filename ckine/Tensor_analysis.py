f3c4b6d6fa620797910de25519a75a775d02a388
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-03-10 17:44:10 -0700

    Update Tensor_analysis.py

--- ckine/Tensor_analysis.py
+++ ckine/Tensor_analysis.py
@@ -3,9 +3,8 @@ Analyze tensor from tensor_generation and plotting.
 """
 import numpy as np
 import tensorly as tl
-from tensorly.decomposition import parafac, non_negative_parafac
-from tensorly.decomposition import tucker
-from tensorly.metrics.regression import variance as tl_var, standard_deviation
+from tensorly.decomposition import non_negative_parafac, non_negative_tucker
+from tensorly.metrics.regression import variance as tl_var
 
 backend = 'numpy' #Only place to choose what the backend should be. numpy = 0. cupy = 1. other backends we desire = 2, ... 
 tl.set_backend(backend) #Set the backend within every file that imports from Tensor_analysis.py
@@ -32,30 +31,12 @@ def perform_decomposition(tensor, r):
 
 def perform_tucker(tensor, rank_list):
     '''Function to peform tucker decomposition.'''
-    out = tucker(z_score_values(tensor), ranks=rank_list) # index 0 is for core tensor, index 1 is for factors; out is a list of core and factors
+    out = non_negative_tucker(z_score_values(tensor), rank_list, tol=1.0E-9, n_iter_max=1000) # index 0 is for core tensor, index 1 is for factors; out is a list of core and factors
     return out
 
 def find_R2X_tucker(values, out):
     '''Compute R2X for the tucker decomposition.'''     
     return R2X(tl.tucker_to_tensor(out[0], out[1]) , z_score_values(values))
-
-def reorient_one(factors, component_index):
-    """Function that takes in the 4 factor matrices and decides if that column index should flip or not and then flips it."""
-    factors_idx = [factors[0][:,component_index], factors[1][:,component_index], factors[2][:,component_index]]
-    component_means = tl.tensor([tl.mean(factors_idx[0]**3), tl.mean(factors_idx[1]**3), tl.mean(factors_idx[2]**3)])
-    if tl.sum(component_means < 0) >= 2 and tl.sum(component_means < 0) <= 3: #if at least 2 are negative, then flip the negative component and keep others unchanged
-        count = 1
-        for index, factor_idx in enumerate(factors_idx):
-            if component_means[index] < 0 and count < 3:
-                factors[index][:, component_index] = factor_idx * -1
-                count += 1
-    return factors
-
-def reorient_factors(factors):
-    """This function is to reorient the factors if at least one component in two factors matrices are negative."""
-    for jj in range(factors[0].shape[1]):
-        factors = reorient_one(factors, jj)
-    return factors
 
 def find_R2X(values, factors):
     '''Compute R2X. Note that the inputs values and factors are in numpy.'''
