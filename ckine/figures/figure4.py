b57d6535691f8ac23d55b409ee65056605ca3edc
adamcweiner <adamcweiner@gmail.com>
2019-04-09 20:12:10 -0700

    bump

--- ckine/figures/figure4.py
+++ ckine/figures/figure4.py
@@ -20,7 +20,6 @@ def makeFigure():
         subplotLabel(item, string.ascii_uppercase[ii])
 
     data_Visterra, cell_names_Visterra = import_Rexpr()
-    print("data_Visterra.shape", data_Visterra.shape)
     unkVec_2_15, scales_2_15 = import_samples_2_15()
     unkVec_4_7, scales_4_7 = import_samples_4_7()
 
