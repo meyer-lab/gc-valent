a445777da22a40e4206edf0955bee637b70306e7
adamcweiner <adamcweiner@gmail.com>
2019-04-10 23:41:50 -0700

    plotting works for trafficking params

--- ckine/figures/figureS5.py
+++ ckine/figures/figureS5.py
@@ -8,20 +8,21 @@ import matplotlib.cm as cm
 import numpy as np
 import os
 from os.path import join
-from .figureCommon import subplotLabel, getSetup
+from .figureCommon import subplotLabel, getSetup, traf_names
 from ..fit import build_model as build_model_2_15
 
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((7, 6), (3, 2))
+    ax, f = getSetup((9, 6), (4, 2))
 
     # Add subplot labels
     for ii, item in enumerate(ax):
         subplotLabel(item, string.ascii_uppercase[ii])
 
-    plot_geweke(ax[0:3], True)
+    plot_geweke(ax[0:4], True)
+    # plot_geweke(ax[4:7], False)
 
     f.tight_layout()
 
@@ -72,3 +73,29 @@ def plot_geweke(ax, traf):
     # plot the scores for scaling constant and kfwd
     ax[2].scatter(score[0]['scales'][:,0], score[0]['scales'][:,1], marker='o', s=25, color='g', label=r'$C_{5}$')
     ax[2].scatter(score[0]['kfwd'][:,0], score[0]['kfwd'][:,1], marker='o', s=25, color='b', label=r'$k_{fwd}$')
+    ax[2].axhline(-1., c='r')
+    ax[2].axhline(1., c='r')
+    ax[2].set_ylim(-1.25,1.25)
+    ax[2].set_xlim(0-10,.5*trace['IL2Raexpr'].shape[0]/2+10)
+    ax[2].set_title('Geweke Plot Comparing first 10% and Slices of the Last 50% of Chain\nDifference in Mean Z scores')
+    ax[2].legend()
+    ax[2].set_xlabel("Position in Chain")
+    ax[2].set_ylabel("Geweke Score")
+
+    if traf is True:
+        colors = cm.rainbow(np.linspace(0, 1, 5))
+        tr_names = traf_names()
+        ax[3].scatter(score[0]['endo'][:,0], score[0]['endo'][:,1], marker='o', s=25, color=colors[0], label=tr_names[0])
+        ax[3].scatter(score[0]['activeEndo'][:,0], score[0]['activeEndo'][:,1], marker='o', s=25, color=colors[1], label=tr_names[1])
+        ax[3].scatter(score[0]['sortF'][:,0], score[0]['sortF'][:,1], marker='o', s=25, color=colors[2], label=tr_names[2])
+        ax[3].scatter(score[0]['kRec'][:,0], score[0]['kRec'][:,1], marker='o', s=25, color=colors[3], label=tr_names[3])
+        ax[3].scatter(score[0]['kDeg'][:,0], score[0]['kDeg'][:,1], marker='o', s=25, color=colors[4], label=tr_names[4])
+        ax[3].axhline(-1., c='r')
+        ax[3].axhline(1., c='r')
+        ax[3].set_ylim(-1.25,1.25)
+        ax[3].set_xlim(0-10,.5*trace['kDeg'].shape[0]/2+10)
+        ax[3].set_title('Geweke Plot Comparing first 10% and Slices of the Last 50% of Chain\nDifference in Mean Trafficking Z scores')
+        ax[3].legend()
+        ax[3].set_xlabel("Position in Chain")
+        ax[3].set_ylabel("Geweke Score")
+
