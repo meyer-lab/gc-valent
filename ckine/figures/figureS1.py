5127e36f55a9c3888940fc360c400b5a8b6efbb3
adamcweiner <adamcweiner@gmail.com>
2019-04-10 23:45:10 -0700

    make figS1 normal again

--- ckine/figures/figureS1.py
+++ ckine/figures/figureS1.py
@@ -2,15 +2,8 @@
 This creates Figure S1.
 """
 import string
-import os
-import pymc3 as pm
-import matplotlib.cm as cm
-import numpy as np
-import os
-from os.path import join
 from .figureCommon import subplotLabel, getSetup, import_samples_2_15, kfwd_info
 from .figure1 import pstat_act, violinPlots, rateComp
-from ..fit import build_model as build_model_2_15
 
 
 def makeFigure():
@@ -29,35 +22,6 @@ def makeFigure():
     rateComp(ax[1], unkVec)
     violinPlots(ax[2:4], unkVec, scales, Fig1=False)
 
-    # plot_geweke(ax[5], True)
-
     f.tight_layout()
 
     return f
-
-def plot_geweke(ax, traf):
-    """ Uses geweke criterion to evaluate model convergence during fitting. """
-    bmodel = build_model_2_15() # access pymc3 object from IL2_IL15 fitting
-    path = os.path.dirname(os.path.abspath(__file__))
-    if traf:
-        trace = pm.backends.text.load(join(path, '../../IL2_model_results'), bmodel.M)
-    else:
-        trace = pm.backends.text.load(join(path, '../../IL2_15_no_traf'), bmodel.M)
-
-    # use use trace to calculate geweke z-scores
-    score = pm.diagnostics.geweke(trace, first=0.1, last=0.5, intervals=20)
-    
-    # plot the scores for rxn rates
-    rxn_len = len(score[0]['rxn'])
-    rxn_names = ['k4rev', 'k5rev', 'k16rev', 'k17rev', 'k22rev', 'k23rev']
-    colors = cm.rainbow(np.linspace(0, 1, rxn_len))
-    for ii in range(rxn_len):
-        ax.scatter(score[0]['rxn'][ii][:,0], score[0]['rxn'][ii][:,1], marker='o', s=25, color=colors[ii], label=rxn_names[ii]) # currently only plotting rxn[0]
-    ax.axhline(-1., c='r')
-    ax.axhline(1., c='r')
-    ax.set_ylim(-1.25,1.25)
-    ax.set_xlim(0-10,.5*trace['rxn'].shape[0]/2+10)
-    ax.set_title('Geweke Plot Comparing first 10% and Slices of the Last 50% of Chain\nDifference in Mean krev Z scores')
-    ax.legend()
-    ax.set_xlabel("Position in Chain")
-    ax.set_ylabel("Geweke Score")
