2ae61030174277b42f51fdb2e819c2c0a9d272b9
Ali <alif@asmlab.org>
2019-04-18 00:48:26 -0700

    linting

--- ckine/figures/figureS5.py
+++ ckine/figures/figureS5.py
@@ -21,7 +21,7 @@ def makeFigure():
 
     for ii, item in enumerate(ax):
         subplotLabel(item, string.ascii_uppercase[ii])  # Add subplot labels
-    
+
     ckineConc, _, IL2_data, IL15_data = import_pstat()
     ckineConc = np.round(np.flip(ckineConc).astype(np.double),5)
     IL2 = np.flip(IL2_data, axis = (0,1)) #Makes them in both chronological order and ascending stimulation concentration
@@ -29,7 +29,7 @@ def makeFigure():
     concat = np.concatenate((IL2,IL15), axis = 1) #Prepare for tensor reshaping
     measured_tensor = np.reshape(concat, (11,4,24))
     cell_names = ['Mem Th', 'Naive Th', 'T helper', 'Mem Treg', 'Naive Treg','Tregs', 'Mem CD8+','Naive CD8+','CD3+CD8+','NKT', 'NK']
-    
+
     factors_activity = []
     for jj in range(measured_tensor.shape[2] - 1):
         factors = perform_decomposition(measured_tensor, jj + 1)
@@ -40,7 +40,6 @@ def makeFigure():
     factors_activ = factors_activity[n_comps - 1] #First dimension is cells. Second is time. Third is ligand. 
     plot_timepoints(ax[1], factors_activ[1]) #Time is the second dimension in this case because reshaping only correctly did 11*4*24
 
-
     plot_cells(ax[2], factors_activ[0], 1, 2, cell_names, ax_pos=1)
 
     plot_ligands(ax[3], factors_activ[2], 1, 2, ckineConc, ax_pos=2)
@@ -50,8 +49,8 @@ def makeFigure():
 
 def plot_timepoints(ax, factors):
     """Function to put all timepoint curves in one figure."""
-    ts = np.array([0.5, 1., 2., 4.])*60.    
-    
+    ts = np.array([0.5, 1., 2., 4.])*60.
+
     colors = ['b', 'k', 'r', 'y', 'm', 'g']
     for ii in range(factors.shape[1]):
         ax.plot(ts, factors[:, ii], c=colors[ii], label='Component ' + str(ii + 1))
@@ -61,7 +60,7 @@ def plot_timepoints(ax, factors):
     ax.set_ylabel('Component')
     ax.set_title('Time')
     ax.legend()
-    
+
 def plot_ligands(ax, factors, component_x, component_y, IL_treat, ax_pos):
     "This function is to plot the ligand combination dimension of the values tensor."
     markers = ['^', '*']
