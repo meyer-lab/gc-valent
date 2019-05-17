eac8628ad5a9e1bc6ba9a116ad1de02776a346fd
zoekim <zoeekimm@gmail.com>
2019-05-17 10:09:25 -0700

    Fix title

--- ckine/figures/figureB4.py
+++ ckine/figures/figureB4.py
@@ -7,7 +7,6 @@ from .figureCommon import subplotLabel, getSetup
 from ..simulation_plotting import solve_IL2_IL15
 
 
-
 def makeFigure():
     """ Build the figure. """
     # Get list of axis objects
@@ -23,15 +22,11 @@ def makeFigure():
     return f
 
 def nonadditive_activity(ax, final_conc, num, time, conc):
+    """ Plots IL2 & IL15 activity versus concentration for a given time point. """
     active_matrix, _, xaxis = solve_IL2_IL15(final_conc, num, time)
     ax.plot(xaxis, active_matrix[0], 'b', label = 'IL2 Activity')
     ax.plot(xaxis, active_matrix[1], 'r', label = 'IL15 Activity')
     ax.plot(xaxis, active_matrix[2], 'k', label = 'Total Activity')
-    ax.set(xlabel='Log(IL2/IL15)',ylabel='Activity',title=('Non-additive effects between IL2/15 in YT-1 cells (',conc,')'))
+    ax.set(xlabel='Log(IL2/IL15)',ylabel='Activity',title='Non-additive effects between IL2/15 in YT-1 cells (' + conc + ')')
     ax.set_ylim(bottom = 0)
     ax.legend()
-    
-    
-    
-    
-    
