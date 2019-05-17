afeea9ddab916a0d1c40fd61ce3969e212c102b7
Peter Emmel <Peter>
2019-05-17 09:53:39 -0700

    Move Non-additive Notebook(#450)

--- ckine/figures/figureB4.py
+++ ckine/figures/figureB4.py
@@ -1,16 +1,37 @@
 """
 This creates Figure 4.
 """
+import matplotlib.pyplot as plt
+import string
 from .figureCommon import subplotLabel, getSetup
+from ..simulation_plotting import solve_IL2_IL15
+
 
 
 def makeFigure():
     """ Build the figure. """
     # Get list of axis objects
-    ax, f = getSetup((7, 6), (3, 4))
+    ax, f = getSetup((7, 6), (2, 2))
 
     subplotLabel(ax[0], 'A')
+
+    nonadditive_activity(ax[0], 1.0, 50, 50.0, '1nM')
+    nonadditive_activity(ax[1], 500.0, 50, 240.0, '500nM')
 
     f.tight_layout()
 
     return f
+
+def nonadditive_activity(ax, final_conc, num, time, conc):
+    active_matrix, _, xaxis = solve_IL2_IL15(final_conc, num, time)
+    ax.plot(xaxis, active_matrix[0], 'b', label = 'IL2 Activity')
+    ax.plot(xaxis, active_matrix[1], 'r', label = 'IL15 Activity')
+    ax.plot(xaxis, active_matrix[2], 'k', label = 'Total Activity')
+    ax.set(xlabel='Log(IL2/IL15)',ylabel='Activity',title=('Non-additive effects between IL2/15 in YT-1 cells (',conc,')'))
+    ax.set_ylim(bottom = 0)
+    ax.legend()
+    
+    
+    
+    
+    
