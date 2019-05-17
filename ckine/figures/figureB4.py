8ec4276571c4bb3210568b4214c04ce6dc440f72
zoekim <zoeekimm@gmail.com>
2019-05-17 10:16:56 -0700

    Linting

--- ckine/figures/figureB4.py
+++ ckine/figures/figureB4.py
@@ -1,8 +1,6 @@
 """
 This creates Figure 4.
 """
-import matplotlib.pyplot as plt
-import string
 from .figureCommon import subplotLabel, getSetup
 from ..simulation_plotting import solve_IL2_IL15
 
@@ -21,12 +19,13 @@ def makeFigure():
 
     return f
 
+
 def nonadditive_activity(ax, final_conc, num, time, conc):
     """ Plots IL2 & IL15 activity versus concentration for a given time point. """
     active_matrix, _, xaxis = solve_IL2_IL15(final_conc, num, time)
-    ax.plot(xaxis, active_matrix[0], 'b', label = 'IL2 Activity')
-    ax.plot(xaxis, active_matrix[1], 'r', label = 'IL15 Activity')
-    ax.plot(xaxis, active_matrix[2], 'k', label = 'Total Activity')
-    ax.set(xlabel='Log(IL2/IL15)',ylabel='Activity',title='Non-additive effects between IL2/15 in YT-1 cells (' + conc + ')')
-    ax.set_ylim(bottom = 0)
+    ax.plot(xaxis, active_matrix[0], 'b', label='IL2 Activity')
+    ax.plot(xaxis, active_matrix[1], 'r', label='IL15 Activity')
+    ax.plot(xaxis, active_matrix[2], 'k', label='Total Activity')
+    ax.set(xlabel='Log(IL2/IL15)', ylabel='Activity', title='Non-additive effects between IL2/15 in YT-1 cells (' + conc + ')')
+    ax.set_ylim(bottom=0)
     ax.legend()
