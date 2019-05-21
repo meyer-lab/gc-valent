157ad2c291c12cd2fc9628722fdb7c2a9b27343c
adamcweiner <adamcweiner@gmail.com>
2019-05-21 13:41:48 -0700

    more results updates

--- ckine/figures/figureCommon.py
+++ ckine/figures/figureCommon.py
@@ -28,8 +28,6 @@ def getSetup(figsize, gridd, mults=None, multz=None, empts=None):
     # Make grid
     gs1 = gridspec.GridSpec(*gridd)
 
-    # ax = []
-
     # Get list of axis objects
     if mults is None:
         ax = [f.add_subplot(gs1[x]) for x in range(gridd[0] * gridd[1]) if x not in empts]
