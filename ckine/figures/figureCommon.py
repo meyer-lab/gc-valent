614967cb3f55f5d4093129e969502ebbdda1e91e
Ali <alif@asmlab.org>
2019-05-22 23:20:20 -0700

    add 7ra to factorization

--- ckine/figures/figureCommon.py
+++ ckine/figures/figureCommon.py
@@ -59,10 +59,11 @@ def set_bounds(ax, compNum):
 def plot_ligands(ax, factors, component_x, component_y, ax_pos, n_ligands, mesh, fig, fig3=True, fig4=False):
     "This function is to plot the ligand combination dimension of the values tensor."
     if not fig4:
-        markers = ['^', '*', '.']
+        markers = ['^', '*', '.', 'd']
         legend_shape = [Line2D([0], [0], color='k', marker=markers[0], label='IL-2', linestyle=''),
                         Line2D([0], [0], color='k', label='IL-2 mut', marker=markers[1], linestyle=''),
-                        Line2D([0], [0], color='k', label='IL-15', marker=markers[2], linestyle='')]
+                        Line2D([0], [0], color='k', label='IL-15', marker=markers[2], linestyle=''),
+                        Line2D([0], [0], color='k', label='IL-7', marker=markers[3], linestyle='')]
         hu = np.around(np.sum(mesh[range(int(mesh.shape[0]/n_ligands)), :], axis=1).astype(float), decimals=7)
 
     else:
