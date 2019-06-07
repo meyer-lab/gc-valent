34a4c3a1bf99b208d70c7ead510ba8c859845381
zoekim <zoeekimm@gmail.com>
2019-06-07 11:39:29 -0700

    Hill curves generated

--- ckine/figures/figure4.py
+++ ckine/figures/figure4.py
@@ -1,9 +1,10 @@
 """
-This creates Figure S5. CP decomposition of measured pSTAT data.
+This creates Figure 4. CP decomposition of measured pSTAT data.
 """
 import string
 import numpy as np
-from scipy.optimize import least_squares
+import math
+from scipy.optimize import least_squares, minimize
 from .figureCommon import subplotLabel, getSetup
 from ..imports import import_pstat
 
@@ -18,17 +19,21 @@ def makeFigure():
     
     ckineConc, cell_names_pstat, IL2_data, _ = import_pstat()
     
-    x0 = [10**-10, 2., 2000.]
+    x0 = [1, 2., 1000.]
+    
+    print(np.log10(ckineConc.astype(np.float)*10**4))
+    
     
     for i, _ in enumerate(cell_names_pstat):
         test = IL2_data[(i * 4):((i + 1) * 4)]
-        nllsq(ax[i], x0, ckineConc.astype(np.float), test[3])
+        test2 = test[3] # add log?
+        nllsq(ax[i], x0, np.log10(ckineConc.astype(np.float)*10**4), test2)
 
     return f
 
 
 def nllsq(ax, x0, xdata, ydata):
-    lsq_res = least_squares(residuals, x0, args=(xdata, ydata), bounds=([10.**-13., 0.],[10.**-7, 5.]), jac='3-point', verbose=1)
+    lsq_res = least_squares(residuals, x0, args=(xdata, ydata), bounds=([0., 0., 0.], [10., 10., 10**5.]), jac='3-point')
     y = hill_equation(xdata, lsq_res.x)
     ax.scatter(xdata, ydata)
     ax.plot(xdata, y)
@@ -37,8 +42,8 @@ def hill_equation(x, x0):
     k = x0[0]
     n = x0[1]
     A = x0[2]
-    xk = np.power(x / x0[0], x0[1])
-    return xk / (1.0 + xk)
+    xk = np.power(x / k, n)
+    return A * xk / (1.0 + xk)
 
 def residuals(x0, x, y):
     return hill_equation(x, x0) - y
