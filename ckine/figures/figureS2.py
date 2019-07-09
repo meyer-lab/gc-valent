36d3254f81eadbd06ee7d95792928cba78992365
adamcweiner <adamcweiner@gmail.com>
2019-07-09 08:53:30 -0700

    checkpoint

--- ckine/figures/figureS2.py
+++ ckine/figures/figureS2.py
@@ -72,45 +72,40 @@ def plot_geweke_2_15(ax, traf):
 
 
 
-def plot_geweke_4_7(ax):
+def plot_geweke_4_7(ax, traf=True):
     """ Generating Geweke plots using the traces from IL-4 and IL-7 fitting to Gonnord data. """
     trace = import_samples_4_7(ret_trace=True)  # return the trace
 
     # use use trace to calculate geweke z-scores
     score = pm.diagnostics.geweke(trace, first=0.1, last=0.5, intervals=20)
 
-    # plot the scores for rxn rates
-    colors = cm.rainbow(np.linspace(0, 1, 2))
+    # take score from the 10th interval in the chain... can change this to a random int later
+    dictt = {r'$k_{27}$': score[0]['k27rev'][10, 1], 
+             r'$k_{33}$': score[0]['k33rev'][10, 1],
+             r'$C_{5}$': score[0]['scales'][0][10, 1],
+             r'$C_{6}$': score[0]['scales'][1][10, 1],
+             r'$k_{fwd}$': score[0]['kfwd'][10, 1]}
 
-    ax[0].scatter(score[0]['k27rev'][:, 0], score[0]['k27rev'][:, 1], marker='o', s=25, color=colors[0], label=r'$k_{27}$')
-    ax[0].scatter(score[0]['k33rev'][:, 0], score[0]['k33rev'][:, 1], marker='o', s=25, color=colors[1], label=r'$k_{33}$')
-    ax[0].axhline(-1., c='r')
-    ax[0].axhline(1., c='r')
-    ax[0].set(ylim=(-1.25, 1.25), xlim=(0 - 10, .5 * trace['k27rev'].shape[0] / 2 + 10),
-              xlabel="Position in Chain", ylabel="Geweke Score")
-    ax[0].set_title('IL-4/-7 model: reverse rxn')
-    ax[0].legend()
+    if traf:  # add the trafficking parameters if necessary & set proper title
+        dictt.update({r'$k_{endo}$': score[0]['endo'][10, 1],
+                     r'$k_{endo,a}$': score[0]['activeEndo'][10, 1],
+                     r'$f_{sort}$': score[0]['sortF'][10, 1],
+                     r'$k_{rec}$': score[0]['kRec'][10, 1],
+                     r'$k_{deg}$': score[0]['kDeg'][10, 1]})
+        ax.set_title(r'IL-4/-7 trafficking model')
+    else:
+        ax.set_title(r'IL-4/-7 no trafficking model')
 
-    # plot the scores for scaling constant and kfwd
-    ax[1].scatter(score[0]['scales'][0][:, 0], score[0]['scales'][0][:, 1], marker='o', s=25, color='g', label=r'$C_{5}$')
-    ax[1].scatter(score[0]['scales'][1][:, 0], score[0]['scales'][1][:, 1], marker='o', s=25, color='c', label=r'$C_{6}$')
-    ax[1].scatter(score[0]['kfwd'][:, 0], score[0]['kfwd'][:, 1], marker='o', s=25, color='b', label=r'$k_{fwd}$')
-    ax[1].axhline(-1., c='r')
-    ax[1].axhline(1., c='r')
-    ax[1].set(ylim=(-1.25, 1.25), xlim=(0 - 10, .5 * trace['kfwd'].shape[0] / 2 + 10),
-              xlabel="Position in Chain", ylabel="Geweke Score")
-    ax[1].set_title(r'IL-4/-7 model: $C_{5}$, $C_{6}$, and $k_{fwd}$')
-    ax[1].legend()
+    df = pd.DataFrame.from_dict(dictt, orient='index')
+    sns.scatterplot(data=np.abs(df), ax=ax)
 
-    colors = cm.rainbow(np.linspace(0, 1, 5))
-    tr_names = traf_names()
-    ax[2].scatter(score[0]['endo'][:, 0], score[0]['endo'][:, 1], marker='o', s=25, color=colors[0], label=tr_names[0])
-    ax[2].scatter(score[0]['activeEndo'][:, 0], score[0]['activeEndo'][:, 1], marker='o', s=25, color=colors[1], label=tr_names[1])
-    ax[2].scatter(score[0]['sortF'][:, 0], score[0]['sortF'][:, 1], marker='o', s=25, color=colors[2], label=r'$f_{sort}$')  # sortF not in traf_names()
-    ax[2].scatter(score[0]['kRec'][:, 0], score[0]['kRec'][:, 1], marker='o', s=25, color=colors[3], label=tr_names[2])
-    ax[2].scatter(score[0]['kDeg'][:, 0], score[0]['kDeg'][:, 1], marker='o', s=25, color=colors[4], label=tr_names[3])
-    ax[2].axhline(-1., c='r')
-    ax[2].axhline(1., c='r')
-    ax[2].set(ylim=(-1.25, 1.25), xlim=(0 - 10, .5 * trace['endo'].shape[0] / 2 + 10),
-              xlabel="Position in Chain", ylabel="Geweke Score", title="IL-4/-7 model: traf rates")
-    ax[2].legend()
+    ax.set_xticklabels(list(dictt.keys()),  # use keys from dict as x-axis labels
+                       rotation=25,
+                       rotation_mode="anchor",
+                       ha="right",
+                       fontsize=8,
+                       position=(0, 0.075))
+    ax.get_legend().set_visible(False)  # remove legend created by sns
+    ax.axhline(1., c='r')  # line to denote acceptable threshold of standard deviations
+    ax.set(ylim=(-0.1, 1.25))
+
