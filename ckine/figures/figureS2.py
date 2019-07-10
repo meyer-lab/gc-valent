9f1ec3dfcca91a905f2479f9bd8271fe3b6e4eb5
adamcweiner <adamcweiner@gmail.com>
2019-07-10 08:57:40 -0700

    switch to largest z-score out of all intervals

--- ckine/figures/figureS2.py
+++ ckine/figures/figureS2.py
@@ -14,15 +14,15 @@ from ..imports import import_samples_2_15, import_samples_4_7
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((12, 6), (3, 4))
+    ax, f = getSetup((7, 6), (3, 1))
 
     # Add subplot labels
     for ii, item in enumerate(ax):
         subplotLabel(item, string.ascii_uppercase[ii])
 
     plot_geweke_2_15(ax[0], True)
-    plot_geweke_2_15(ax[4], False)
-    plot_geweke_4_7(ax[8:11])
+    plot_geweke_2_15(ax[1], False)
+    plot_geweke_4_7(ax[2])
 
     return f
 
@@ -34,25 +34,25 @@ def plot_geweke_2_15(ax, traf):
     # use use trace to calculate geweke z-scores
     score = pm.diagnostics.geweke(trace, first=0.1, last=0.5, intervals=20)
 
-    # take score from the 10th interval in the chain... can change this to a random int later
-    dictt = {r'$k_{4}$': score[0]['rxn'][0][10, 1], 
-             r'$k_{5}$': score[0]['rxn'][1][10, 1],
-             r'$k_{16}$': score[0]['rxn'][2][10, 1],
-             r'$k_{17}$': score[0]['rxn'][3][10, 1],
-             r'$k_{22}$': score[0]['rxn'][4][10, 1],
-             r'$k_{23}$': score[0]['rxn'][5][10, 1],
-             'IL-2Rα': score[0]['Rexpr_2Ra'][10, 1],
-             'IL-2Rβ': score[0]['Rexpr_2Rb'][10, 1],
-             'IL-15Rα': score[0]['Rexpr_15Ra'][10, 1],
-             r'$C_{5}$': score[0]['scales'][10, 1],
-             r'$k_{fwd}$': score[0]['kfwd'][10, 1]}
+    # take z-score from interval with maximum absolute value (i.e. the interval that converged the worst)
+    dictt = {r'$k_{4}$': max(abs(min(score[0]['rxn'][0][:, 1])), max(score[0]['rxn'][0][:, 1])),
+             r'$k_{5}$': max(abs(min(score[0]['rxn'][1][:, 1])), max(score[0]['rxn'][1][:, 1])),
+             r'$k_{16}$': max(abs(min(score[0]['rxn'][2][:, 1])), max(score[0]['rxn'][2][:, 1])),
+             r'$k_{17}$': max(abs(min(score[0]['rxn'][3][:, 1])), max(score[0]['rxn'][3][:, 1])),
+             r'$k_{22}$': max(abs(min(score[0]['rxn'][4][:, 1])), max(score[0]['rxn'][4][:, 1])),
+             r'$k_{23}$': max(abs(min(score[0]['rxn'][5][:, 1])), max(score[0]['rxn'][5][:, 1])),
+             'IL-2Rα': max(abs(min(score[0]['Rexpr_2Ra'][:, 1])), max(score[0]['Rexpr_2Ra'][:, 1])),
+             'IL-2Rβ': max(abs(min(score[0]['Rexpr_2Rb'][:, 1])), max(score[0]['Rexpr_2Rb'][:, 1])),
+             'IL-15Rα': max(abs(min(score[0]['Rexpr_15Ra'][:, 1])), max(score[0]['Rexpr_15Ra'][:, 1])),
+             r'$C_{5}$': max(abs(min(score[0]['scales'][:, 1])), max(score[0]['scales'][:, 1])),
+             r'$k_{fwd}$': max(abs(min(score[0]['kfwd'][:, 1])), max(score[0]['kfwd'][:, 1])),}
 
     if traf:  # add the trafficking parameters if necessary & set proper title
-        dictt.update({r'$k_{endo}$': score[0]['endo'][10, 1],
-                     r'$k_{endo,a}$': score[0]['activeEndo'][10, 1],
-                     r'$f_{sort}$': score[0]['sortF'][10, 1],
-                     r'$k_{rec}$': score[0]['kRec'][10, 1],
-                     r'$k_{deg}$': score[0]['kDeg'][10, 1]})
+        dictt.update({r'$k_{endo}$': max(abs(min(score[0]['endo'][:, 1])), max(score[0]['endo'][:, 1])),
+                     r'$k_{endo,a}$': max(abs(min(score[0]['activeEndo'][:, 1])), max(score[0]['activeEndo'][:, 1])),
+                     r'$f_{sort}$': max(abs(min(score[0]['sortF'][:, 1])), max(score[0]['sortF'][:, 1])),
+                     r'$k_{rec}$': max(abs(min(score[0]['kRec'][:, 1])), max(score[0]['kRec'][:, 1])),
+                     r'$k_{deg}$': max(abs(min(score[0]['kDeg'][:, 1])), max(score[0]['kDeg'][:, 1]))})
         ax.set_title(r'IL-2/-15 trafficking model')
     else:
         ax.set_title(r'IL-2/-15 no trafficking model')
@@ -68,7 +68,7 @@ def plot_geweke_2_15(ax, traf):
                        position=(0, 0.075))
     ax.get_legend().set_visible(False)  # remove legend created by sns
     ax.axhline(1., c='r')  # line to denote acceptable threshold of standard deviations
-    ax.set(ylim=(-0.1, 1.25))
+    ax.set(ylim=(-0.1, 1.25), ylabel=r"max|$z-score$|")
 
 
 
@@ -80,18 +80,18 @@ def plot_geweke_4_7(ax, traf=True):
     score = pm.diagnostics.geweke(trace, first=0.1, last=0.5, intervals=20)
 
     # take score from the 10th interval in the chain... can change this to a random int later
-    dictt = {r'$k_{27}$': score[0]['k27rev'][10, 1], 
-             r'$k_{33}$': score[0]['k33rev'][10, 1],
-             r'$C_{5}$': score[0]['scales'][0][10, 1],
-             r'$C_{6}$': score[0]['scales'][1][10, 1],
-             r'$k_{fwd}$': score[0]['kfwd'][10, 1]}
+    dictt = {r'$k_{27}$': max(abs(min(score[0]['k27rev'][:, 1])), max(score[0]['k27rev'][:, 1])),
+             r'$k_{33}$': max(abs(min(score[0]['k33rev'][:, 1])), max(score[0]['k33rev'][:, 1])),
+             r'$C_{5}$': max(abs(min(score[0]['scales'][0][:, 1])), max(score[0]['scales'][0][:, 1])),
+             r'$C_{6}$': max(abs(min(score[0]['scales'][1][:, 1])), max(score[0]['scales'][1][:, 1])),
+             r'$k_{fwd}$': max(abs(min(score[0]['kfwd'][:, 1])), max(score[0]['kfwd'][:, 1]))}
 
     if traf:  # add the trafficking parameters if necessary & set proper title
-        dictt.update({r'$k_{endo}$': score[0]['endo'][10, 1],
-                     r'$k_{endo,a}$': score[0]['activeEndo'][10, 1],
-                     r'$f_{sort}$': score[0]['sortF'][10, 1],
-                     r'$k_{rec}$': score[0]['kRec'][10, 1],
-                     r'$k_{deg}$': score[0]['kDeg'][10, 1]})
+        dictt.update({r'$k_{endo}$': max(abs(min(score[0]['endo'][:, 1])), max(score[0]['endo'][:, 1])),
+                     r'$k_{endo,a}$': max(abs(min(score[0]['activeEndo'][:, 1])), max(score[0]['activeEndo'][:, 1])),
+                     r'$f_{sort}$': max(abs(min(score[0]['sortF'][:, 1])), max(score[0]['sortF'][:, 1])),
+                     r'$k_{rec}$': max(abs(min(score[0]['kRec'][:, 1])), max(score[0]['kRec'][:, 1])),
+                     r'$k_{deg}$': max(abs(min(score[0]['kDeg'][:, 1])), max(score[0]['kDeg'][:, 1]))})
         ax.set_title(r'IL-4/-7 trafficking model')
     else:
         ax.set_title(r'IL-4/-7 no trafficking model')
