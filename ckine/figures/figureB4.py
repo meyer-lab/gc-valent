3e0e05bdaab25cc1f3ddb8650b22ea9bba43ee11
Aaron Meyer <ameyer@ucla.edu>
2019-07-25 13:04:20 -0700

    Upload figure.

--- ckine/figures/figureB4.py
+++ ckine/figures/figureB4.py
@@ -1,6 +1,13 @@
 """
 This creates Figure 4.
 """
+import logging
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from ckine.imports import import_muteins
+from ckine.tensor import perform_decomposition, find_R2X, z_score_values
+from ckine.figures.figureCommon import plot_cells
 from .figureCommon import subplotLabel, getSetup
 
 
@@ -9,6 +16,31 @@ def makeFigure():
     # Get list of axis objects
     ax, f = getSetup((7, 6), (2, 2))
 
-    subplotLabel(ax[0], 'A')
+    for ii, item in enumerate(ax):
+        subplotLabel(item, string.ascii_uppercase[ii])
+
+    dataMean, dataTensor = import_muteins()
+    dataMean.reset_index(inplace=True)
+
+    concs = dataMean['Concentration'].unique()
+    ts = dataMean['Time'].unique()
+    cells = dataMean['Cells'].unique()
+    ligs = dataMean['Ligand'].unique()
+
+    dataTensor = z_score_values(dataTensor, 0)
+    parafac = perform_decomposition(dataTensor, 2, weightFactor=3)
+    logging.info(find_R2X(dataTensor, parafac))
+    
+    # Cells
+    plot_cells(ax[0], parafac[1], 1, 2, ligs)
+    
+    # Ligands
+    plot_cells(ax[1], parafac[1], 1, 2, ligs)
+    
+    # Timepoints
+    ax[2].plot(ts, parafac[2]);
+    
+    # Concentration
+    ax[3].semilogx(concs, parafac[3]);
 
     return f
