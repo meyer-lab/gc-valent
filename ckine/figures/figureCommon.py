690c97a3e6ca5e105db5e0b30f95a0b8f3260efc
adamcweiner <adamcweiner@gmail.com>
2019-07-29 07:15:56 -0700

    add buffer to ylim of R2X plots

--- ckine/figures/figureCommon.py
+++ ckine/figures/figureCommon.py
@@ -77,7 +77,7 @@ def plot_R2X(ax, tensor, factors_list):
     ax.plot(range(1, len(factors_list) + 1), R2X_array, 'ko', label='Overall R2X')
     ax.set_ylabel('R2X')
     ax.set_xlabel('Number of Components')
-    ax.set_ylim(0, 1)
+    ax.set_ylim(0, 1.05)
     ax.set_xticks(np.arange(1, len(factors_list) + 1))
     ax.set_xticklabels(np.arange(1, len(factors_list) + 1))
 
