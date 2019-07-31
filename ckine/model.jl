0cd14c632969fc90b4cd49b44e204f65d0074579
Aaron Meyer <git@asmlab.org>
2019-07-31 09:15:00 -0700

    Change indent.

--- ckine/model.jl
+++ ckine/model.jl
@@ -1,67 +1,67 @@
 using DifferentialEquations
 using Sundials
-	
+    
 include("reaction.jl")
 
 function runCkine(tps, params, sensi, IL2case)
-	if IL2case
-		ILs, surface, endosome, trafP = IL2param(params)
-		f = IL2Deriv
-	else
-		ILs, surface, endosome, trafP = fullParam(params)
-		f = fullDeriv
-	end
+    if IL2case
+        ILs, surface, endosome, trafP = IL2param(params)
+        f = IL2Deriv
+    else
+        ILs, surface, endosome, trafP = fullParam(params)
+        f = fullDeriv
+    end
 
-	u0 = solveAutocrine(trafP)
+    u0 = solveAutocrine(trafP)
 
-	if sensi
-		prob = ODELocalSensitivityProblem(f, u0, (0.0, maximum(tps)), params)
-	else
-		prob = ODEProblem(f, u0, (0.0, maximum(tps)), params)
-	end
+    if sensi
+        prob = ODELocalSensitivityProblem(f, u0, (0.0, maximum(tps)), params)
+    else
+        prob = ODEProblem(f, u0, (0.0, maximum(tps)), params)
+    end
 
-	sol = solve(prob, CVODE_BDF(linear_solver=:Dense))
+    sol = solve(prob, CVODE_BDF(linear_solver=:Dense))
 
-	if sensi
-		return adjoint_sensitivities(sol, alg, dg, tps)
-	else
-		return sol(tps), nothing
-	end
+    if sensi
+        return adjoint_sensitivities(sol, alg, dg, tps)
+    else
+        return sol(tps), nothing
+    end
 end
 
 
 function runCkinePretreat(pret, tt, params, postStim)
-	ILs, surface, endosome, trafP = fullParam(params)
+    ILs, surface, endosome, trafP = fullParam(params)
 
-	u0 = solveAutocrine(trafP)
+    u0 = solveAutocrine(trafP)
 
-	prob = ODEProblem(fullDeriv, u0, (0.0, pret), params)
+    prob = ODEProblem(fullDeriv, u0, (0.0, pret), params)
 
-	sol = solve(prob, CVODE_BDF())
+    sol = solve(prob, CVODE_BDF())
 
-	# Set ILs in params
-	params[1:6] = postStim
+    # Set ILs in params
+    params[1:6] = postStim
 
-	prob = ODEProblem(fullDeriv, u0, (0.0, tt), params)
+    prob = ODEProblem(fullDeriv, u0, (0.0, tt), params)
 
-	sol = solve(prob, CVODE_BDF())
+    sol = solve(prob, CVODE_BDF())
 
-	return sol(tt)
+    return sol(tt)
 end
 
 
 function runCkineParallel(rxnRatesIn, tp, sensi)
-	#u0 = solveAutocrine(trafP)
-	#tspan = (0.0, max(tps))
+    #u0 = solveAutocrine(trafP)
+    #tspan = (0.0, max(tps))
 
-	#if sensi
-	#	prob = ODELocalSensitivityProblem(f, u0, tspan, p)
-	#else
-	#	prob = ODEProblem(f, u0, tspan, p)
-	#end
+    #if sensi
+    #   prob = ODELocalSensitivityProblem(f, u0, tspan, p)
+    #else
+    #   prob = ODEProblem(f, u0, tspan, p)
+    #end
 
-	#sol = solve(prob, Tsit5())
-	#t = collect(tps)
+    #sol = solve(prob, Tsit5())
+    #t = collect(tps)
 
-	return out, sensiOut
+    return out, sensiOut
 end
