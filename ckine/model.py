71707a25b0bfcb1ee1d18905214c939a647cf5a0
Aaron Meyer <ameyer@ucla.edu>
2019-08-22 15:22:30 -0700

    Add assert.

--- ckine/model.py
+++ ckine/model.py
@@ -66,9 +66,10 @@ def runCkineU_IL2(tps, rxntfr):
     """ Standard version of solver that returns species abundances given times and unknown rates. """
     rxntfr = rxntfr.copy()
     assert rxntfr.size == 15
+    assert np.all(rxntfr >= 0.0)
 
     yOut = np.zeros((tps.size, __nSpecies), dtype=np.float64)
-    
+
     retVal = libb.runCkine(tps.ctypes.data_as(ct.POINTER(ct.c_double)), tps.size, yOut.ctypes.data_as(ct.POINTER(ct.c_double)), rxntfr.ctypes.data_as(ct.POINTER(ct.c_double)), True, 0.0, None)
 
     assert retVal >= 0  # make sure solver worked
@@ -189,7 +190,7 @@ def getActiveCytokine(cytokineIDX, yVec):
 def getTotalActiveCytokine(cytokineIDX, yVec):
     """ Get amount of surface and endosomal active species. """
     assert yVec.ndim == 1
-    return getActiveCytokine(cytokineIDX, yVec[0:__halfL]) + __internalStrength * getActiveCytokine(cytokineIDX, yVec[__halfL: __halfL * 2])
+    return getActiveCytokine(cytokineIDX, yVec[0:__halfL]) + __internalStrength * getActiveCytokine(cytokineIDX, yVec[__halfL : __halfL * 2])
 
 
 def surfaceReceptors(y):
@@ -207,13 +208,13 @@ def surfaceReceptors(y):
 
 def totalReceptors(yVec):
     """This function takes in a vector y and returns the amounts of all 8 receptors in both cell compartments"""
-    return surfaceReceptors(yVec) + __internalStrength * surfaceReceptors(yVec[__halfL: __halfL * 2])
+    return surfaceReceptors(yVec) + __internalStrength * surfaceReceptors(yVec[__halfL : __halfL * 2])
 
 
 def ligandDeg(yVec, sortF, kDeg, cytokineIDX):
     """ This function calculates rate of total ligand degradation. """
-    yVec_endo_species = yVec[__halfL: (__halfL * 2)].copy()  # get all endosomal complexes
-    yVec_endo_lig = yVec[(__halfL * 2)::].copy()  # get all endosomal ligands
+    yVec_endo_species = yVec[__halfL : (__halfL * 2)].copy()  # get all endosomal complexes
+    yVec_endo_lig = yVec[(__halfL * 2) : :].copy()  # get all endosomal ligands
     sum_active = np.sum(getActiveCytokine(cytokineIDX, yVec_endo_species))
     __cytok_species_IDX = np.zeros(__halfL, dtype=np.bool)  # create array of size halfL
     __cytok_species_IDX[getCytokineSpecies()[cytokineIDX]] = 1  # assign 1's for species corresponding to the cytokineIDX
