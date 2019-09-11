7a7c592e6fed7a910344184fa7f3319f06b57b43
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-09-10 18:23:25 -0700

    Update solver.jl

--- ckine/solver.jl
+++ ckine/solver.jl
@@ -1,6 +1,5 @@
 using gcSolver
 
-
 @Base.ccallable function runCkineC(tpsIn::Ptr{Cdouble}, nTps::Csize_t, outt::Ptr{Cdouble}, params::Ptr{Cdouble}, paramsLen::Csize_t)::Cint
     vecc = unsafe_wrap(Vector{Float64}, params, paramsLen)
     tps = unsafe_wrap(Vector{Float64}, tpsIn, nTps)
