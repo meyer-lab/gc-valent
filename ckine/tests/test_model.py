fa5937ad3f6707bd6e7d7bfbbec600b8497128e2
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-09-13 14:26:48 -0700

    Update test_model.py

--- ckine/tests/test_model.py
+++ ckine/tests/test_model.py
@@ -3,26 +3,8 @@ Unit test file.
 """
 import unittest
 import numpy as np
-from hypothesis import given, settings
-from hypothesis.strategies import floats
-from hypothesis.extra.numpy import arrays as harrays
-from ..model import fullModel, getTotalActiveCytokine, runCkineU, nSpecies, runCkineUP, runCkineU_IL2, ligandDeg
+from ..model import getTotalActiveCytokine, runCkineU, runCkineU_IL2, ligandDeg
 from ..figures.figureB1 import runIL2simple
-
-
-settings.register_profile("ci", max_examples=1000, deadline=None)
-settings.load_profile("ci")
-
-conservation_IDX = [
-    np.array([1, 4, 5, 7, 8, 11, 12, 14, 15]),  # IL2Rb
-    np.array([0, 3, 5, 6, 8]),  # IL2Ra
-    np.array([9, 10, 12, 13, 15]),  # IL15Ra
-    np.array([16, 17, 18]),  # IL7Ra
-    np.array([19, 20, 21]),  # IL9R
-    np.array([22, 23, 24]),  # IL4Ra
-    np.array([25, 26, 27]),  # IL21Ra
-    np.array([2, 6, 7, 8, 13, 14, 15, 18, 21, 24, 27]),
-]  # gc
 
 
 class TestModel(unittest.TestCase):
