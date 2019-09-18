0cf9018ad72190f3121f909d259fa72fdcc10c13
Aaron Meyer <ameyer@ucla.edu>
2019-09-17 21:21:01 -0700

    More.

--- ckine/fit_visterra.py
+++ ckine/fit_visterra.py
@@ -5,7 +5,28 @@ import numpy as np
 from .model import getTotalActiveSpecies, receptor_expression
 from .differencing_op import runCkineDoseOp
 from .imports import import_Rexpr, import_pstat
-from .fit import commonTraf
+
+
+def commonTraf(trafficking=True):
+    """ Set the common trafficking parameter priors. """
+    kfwd = pm.Lognormal('kfwd', mu=np.log(0.001), sd=0.5, shape=1)
+
+    if trafficking:
+        endo = pm.Lognormal('endo', mu=np.log(0.1), sd=0.1, shape=1)
+        activeEndo = pm.Lognormal('activeEndo', sd=0.1, shape=1)
+        kRec = pm.Lognormal('kRec', mu=np.log(0.1), sd=0.1, shape=1)
+        kDeg = pm.Lognormal('kDeg', mu=np.log(0.01), sd=0.2, shape=1)
+        sortF = pm.Beta('sortF', alpha=12, beta=80, shape=1)
+    else:
+        # Assigning trafficking to zero to fit without trafficking
+        endo = activeEndo = kRec = kDeg = T.zeros(1, dtype=np.float64)
+        sortF = T.ones(1, dtype=np.float64) * 0.5
+    return kfwd, endo, activeEndo, kRec, kDeg, sortF
+
+
+def sampling(M):
+    """ This is the sampling that actually runs the model. """
+    return pm.sample(init="ADVI", chains=2, model=M)
 
 
 class pSTAT_activity:  # pylint: disable=too-few-public-methods
@@ -28,32 +49,30 @@ class pSTAT_activity:  # pylint: disable=too-few-public-methods
         Op = runCkineDoseOp(tt=self.ts, condense=getTotalActiveSpecies().astype(np.float64), conditions=self.cytokM)
 
         output = list()
-        jj = 0
+        print(self.cell_names_pstat)
         for i, name in enumerate(self.cell_names_pstat):
-            if name in ("NK", "CD8+", "Mem CD8+", "Naive Treg"):
-                # plot matching experimental and predictive pSTAT data for the same cell type
-                j = self.cell_names_receptor.get_loc(self.cell_names_pstat[i])
+            # plot matching experimental and predictive pSTAT data for the same cell type
+            j = self.cell_names_receptor.get_loc(self.cell_names_pstat[i])
 
-                unkVec = T.set_subtensor(unkVec[16], receptor_expression(self.receptor_data[j, 0], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # IL2Ra
-                unkVec = T.set_subtensor(unkVec[17], receptor_expression(self.receptor_data[j, 1], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # IL2Rb
-                unkVec = T.set_subtensor(unkVec[18], receptor_expression(self.receptor_data[j, 2], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # gc
-                unkVec = T.set_subtensor(unkVec[19], receptor_expression(self.receptor_data[j, 3], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # IL15Ra
-                actVec = Op(unkVec)
+            unkVec = T.set_subtensor(unkVec[16], receptor_expression(self.receptor_data[j, 0], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # IL2Ra
+            unkVec = T.set_subtensor(unkVec[17], receptor_expression(self.receptor_data[j, 1], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # IL2Rb
+            unkVec = T.set_subtensor(unkVec[18], receptor_expression(self.receptor_data[j, 2], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # gc
+            unkVec = T.set_subtensor(unkVec[19], receptor_expression(self.receptor_data[j, 3], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))  # IL15Ra
+            actVec = Op(unkVec)
 
-                # account for pSTAT5 saturation and then normalize from 0 to 1
-                actVec = actVec / (actVec + scale[jj])
-                actVec = actVec / T.mean(actVec)
+            # account for pSTAT5 saturation and then normalize from 0 to 1
+            actVec = actVec / (actVec + scale[i])
+            actVec = actVec / T.mean(actVec)
 
-                # concatenate the cell's IL-2 data to the IL-15 data so it matches actVec
-                data_cat = np.concatenate((self.IL2_data[(i * 4): ((i + 1) * 4)], self.IL15_data[(i * 4): ((i + 1) * 4)]))
-                data_cat = data_cat / np.mean(data_cat)
+            # concatenate the cell's IL-2 data to the IL-15 data so it matches actVec
+            data_cat = np.concatenate((self.IL2_data[(i * 4): ((i + 1) * 4)], self.IL15_data[(i * 4): ((i + 1) * 4)]))
+            data_cat = data_cat / np.mean(data_cat)
 
-                newVec = T.reshape(actVec, data_cat.shape)
+            newVec = T.reshape(actVec, data_cat.shape)
 
-                output.append(newVec - data_cat)  # copy residuals into list
-                jj = jj + 1
+            output.append(newVec - data_cat)  # copy residuals into list
 
-        return output
+        return output, self.cell_names_pstat
 
 
 class build_model:
@@ -73,26 +92,19 @@ class build_model:
 
             rxnrates = pm.Lognormal("rxn", sd=0.5, shape=6)  # 6 reverse rxn rates for IL2/IL15
             nullRates = T.ones(4, dtype=np.float64)  # k27rev, k31rev, k33rev, k35rev
-            scale = pm.Lognormal("scales", mu=np.log(100.0), sd=1, shape=4)  # order: Naive Treg, NK, CD8+, Mem CD8+
+            scale = pm.Lognormal("scales", mu=np.log(100.0), sd=1, shape=10)
 
             # plug in 0 for all receptor rates... will override with true measurements in act.calc()
             Rexpr = T.zeros(4, dtype=np.float64)
 
             unkVec = T.concatenate((kfwd, rxnrates, nullRates, endo, activeEndo, sortF, kRec, kDeg, Rexpr, nullRates * 0.0))
 
-            Y_act = self.act.calc(unkVec, scale)  # fitting the data based on dst15.calc for the given parameters
-
-            sd_act = T.std(Y_act[0])  # Add bounds for the stderr to help force the fitting solution
-            pm.Deterministic("Y_act0", T.sum(T.square(Y_act[0])))
-            pm.Normal("fitD_act0", sd=sd_act, observed=Y_act[0])  # experimental-derived stderr is used
-
-            sd_act = T.std(Y_act[1])  # Add bounds for the stderr to help force the fitting solution
-            pm.Deterministic("Y_act1", T.sum(T.square(Y_act[1])))
-            pm.Normal("fitD_act1", sd=sd_act, observed=Y_act[1])  # experimental-derived stderr is used
-
-            sd_act = T.std(Y_act[2])  # Add bounds for the stderr to help force the fitting solution
-            pm.Deterministic("Y_act2", T.sum(T.square(Y_act[2])))
-            pm.Normal("fitD_act2", sd=sd_act, observed=Y_act[2])  # experimental-derived stderr is used
+            Y_act, names = self.act.calc(unkVec, scale)  # fitting the data based on dst15.calc for the given parameters
+            
+            for ii in range(len(Y_act)):
+                sd_act = T.std(Y_act[ii])  # Add bounds for the stderr to help force the fitting solution
+                pm.Deterministic("Y_act_" + names[ii], T.sum(T.square(Y_act[ii])))
+                pm.Normal("fitD_act_" + names[ii], sd=sd_act, observed=Y_act[ii])  # experimental-derived stderr is used
 
             # Save likelihood
             pm.Deterministic("logp", M.logpt)
