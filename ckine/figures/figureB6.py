99ffebf39c51c8b565dae57fdb0cc04f0053b2c6
zoekim <zoeekimm@gmail.com>
2019-09-17 17:34:07 -0700

    Linting

--- ckine/figures/figureB6.py
+++ ckine/figures/figureB6.py
@@ -129,7 +129,8 @@ def organize_expr_pred(df, cell_name, ligand_name, receptors, muteinC, tps):
         cell_receptors = receptor_expression(receptors, unkVec_2_15[17, j], unkVec_2_15[20, j], unkVec_2_15[19, j], unkVec_2_15[21, j])
         pred_data[:, :, j] = calc_dose_response_mutein(unkVec_2_15[:, j], mutaff[ligand_name], tps, muteinC, cell_receptors)
         df_pred = pd.DataFrame({'Cells': np.tile(np.array(cell_name), num), 'Ligand': np.tile(np.array(ligand_name), num), 'Time Point': np.tile(
-            tps, 12), 'Concentration': mutein_conc.reshape(num,), 'Activity Type': np.tile(np.array('predicted'), num), 'Replicate': np.tile(np.array(j + 1), num), 'Activity': pred_data[:, :, j].reshape(num,)})
+            tps, 12), 'Concentration': mutein_conc.reshape(num,),
+            'Activity Type': np.tile(np.array('predicted'), num), 'Replicate': np.tile(np.array(j + 1), num), 'Activity': pred_data[:, :, j].reshape(num,)})
         df = df.append(df_pred, ignore_index=True)
 
     return df
