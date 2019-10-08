948f1d4fdda6103240f7be701b952bff3714d0ff
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2019-10-07 18:18:40 -0700

    Final Lint I Hope

--- ckine/figures/figureC1.py
+++ ckine/figures/figureC1.py
@@ -20,32 +20,39 @@ def makeFigure():
 
     dose_ind = np.array([0., 6., 11.])
     Tsample, _ = importF("/home/brianoj/Tplate418", "C")
-    _, pstat_arrayT, xf_arrayT, loadingT = pcaAll(Tsample, Tcells=True) #take out titles req
+    _, pstat_arrayT, _, loadingT = pcaAll(Tsample, Tcells=True) #take out titles req
     dataT, _, _ = sampleT(Tsample[0])
     PCAobjT, _ = fitPCA(dataT, Tcells=True)
-    
+
     Nksample, _ = importF("/home/brianoj/Nkplate418", "C")
-    _, pstat_arrayNk, xf_arrayNk, loadingNk = pcaAll(Nksample, Tcells=False) #take out titles req
+    _, pstat_arrayNk, _, loadingNk = pcaAll(Nksample, Tcells=False) #take out titles req
     dataNk, _, _ = sampleNK(Nksample[0])
     PCAobjNk, _ = fitPCA(dataNk, Tcells=False)
-    
-    for i, col in enumerate (dose_ind): 
+
+    for i, col in enumerate(dose_ind):
         col = int(col)
         dataT, _, _ = sampleT(Tsample[col])
         xfT = appPCA(dataT, PCAobjT, Tcells=True)
-        pcaPlt(xfT, pstat_arrayT[col], ax[i], Tcells=True) 
-        
+        pcaPlt(xfT, pstat_arrayT[col], ax[i], Tcells=True)
+
         dataNk, _, _ = sampleNK(Nksample[col])
         xfNk = appPCA(dataNk, PCAobjNk, Tcells=False)
         pcaPlt(xfNk, pstat_arrayNk[col], ax[i + 4], Tcells=False)
 
-    loadingPlot(loadingT, ax = ax[3], Tcells=True)
-    loadingPlot(loadingNk, ax = ax[7], Tcells=False)
+    loadingPlot(loadingT, ax=ax[3], Tcells=True)
+    loadingPlot(loadingNk, ax=ax[7], Tcells=False)
 
-    _, _, _, _, colormatT = pcaAllCellType(Tsample, Tcells=True)
-    _, _, _, _, colormatNK = pcaAllCellType(Nksample, Tcells=False) #take out titles req
-
-    pcaPltColor(xf_arrayT[4], colormatT[4], ax=ax[8], Tcells=True)
-    pcaPltColor(xf_arrayNk[4], colormatNK[4], ax=ax[9], Tcells=False)
+    ColPlot(Tsample, ax, 4, True)
+    ColPlot(Nksample, ax, 4, False)
 
     return f
+
+def ColPlot(sample, ax, col, Tcells=True):
+    if Tcells:
+        _, _, xf_arrayT, _ = pcaAll(sample, Tcells=True)
+        _, _, _, _, colormatT = pcaAllCellType(sample, Tcells=True)
+        pcaPltColor(xf_arrayT[col], colormatT[col], ax=ax[8], Tcells=True)
+    else:
+        _, _, xf_arrayNk, _ = pcaAll(sample, Tcells=False)
+        _, _, _, _, colormatNk = pcaAllCellType(sample, Tcells=False)
+        pcaPltColor(xf_arrayNk[col], colormatNk[col], ax=ax[9], Tcells=False)
