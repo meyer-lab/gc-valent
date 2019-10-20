dc9521b4921d52244f189ad43cf4e8c92a19d040
zoekim <zoeekimm@gmail.com>
2019-10-20 16:22:51 -0700

    Debug

--- ckine/figures/figureB6.py
+++ ckine/figures/figureB6.py
@@ -109,28 +109,6 @@ def plot_expr_pred(ax, df, scales, cell_order, ligand_order, tps, muteinC):
             ax[axis].set(xlabel=("[" + ligand_name + "] (log$_{10}$[nM])"), ylabel="Activity", title=cell_name)
 
 
-def calc_dose_response_mutein(unkVec, input_params, tps, muteinC, mutein_name, cell_receptors):
-    """ Calculates activity for a given cell type at various mutein concentrations and timepoints. """
-
-    total_activity = np.zeros((len(muteinC), len(tps)))
-
-    # loop for each mutein concentration
-    for i, conc in enumerate(muteinC):
-        if mutein_name == 'IL15':
-            unkVec[1] = conc
-            unkVec[22:25] = cell_receptors  # set receptor expression for IL2Ra, IL2Rb, gc
-            unkVec[25] = 0.0  # we never observed any IL-15Ra
-            yOut = runCkineU(tps, unkVec)
-            active_ckine = np.zeros(yOut.shape[0])
-            for j in range(yOut.shape[0]):
-                active_ckine[j] = getTotalActiveCytokine(1, yOut[j, :])
-        else:
-            active_ckine = runIL2simple(unkVec, input_params, conc, tps=tps, input_receptors=cell_receptors)
-        total_activity[i, :] = np.reshape(active_ckine, (-1, 4))  # save the activity from this concentration for all 4 tps
-
-    return total_activity
-
-
 def plot_dose_response(ax, mutein_activity, tps, muteinC):
     """ Plots predicted activity for multiple timepoints and mutein concentrations. """
     colors = cm.rainbow(np.linspace(0, 1, tps.size))
