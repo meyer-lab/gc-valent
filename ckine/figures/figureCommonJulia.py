4e19fdb88a6bc177c354849ba584608456e24acf
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-05-06 10:59:49 -0700

    Add Wrappers

---
+++ ckine/figures/figureCommonJulia.py
@@ -0,0 +1,10 @@
+"""
+This file contains functions that are used in multiple figures from Julia.
+"""
+from julia.api import Julia
+jl = Julia(compiled_modules=False)
+from julia import gcSolver
+
+
+def getUnkVecPy():
+    return gcSolver.getUnkVec()
