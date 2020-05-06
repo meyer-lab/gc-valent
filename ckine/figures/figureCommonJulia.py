556145e3454c9c411b15e1321951c4b2addf83d0
Aaron Meyer <aarmey@users.noreply.github.com>
2020-05-06 19:08:07 +0000

    autopep8 action fixes

--- ckine/figures/figureCommonJulia.py
+++ ckine/figures/figureCommonJulia.py
@@ -1,9 +1,9 @@
 """
 This file contains functions that are used in multiple figures from Julia.
 """
+from julia import gcSolver
 from julia.api import Julia
 jl = Julia(compiled_modules=False)
-from julia import gcSolver
 
 
 def getUnkVecPy():
