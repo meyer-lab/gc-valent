cae200249be93220defbd31dbf182f41542a9df0
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-06-29 19:27:47 -0700

    More Debug

--- ckine/figures/figureC5.py
+++ ckine/figures/figureC5.py
@@ -51,8 +51,12 @@ def makeFigure():
                     for k, signal in enumerate(data):
                         A, B, C, D = lsq_params[j]
                         rec_counts[k] = C * (((A - D) / (signal - D)) - 1)**(1 / B)
+                        print(cell)
+                        print(receptor)
+                        print(signal)
                     df_add = pd.DataFrame({"Cell Type": np.tile(cell, len(data)), "Receptor": np.tile(receptor, len(data)),
                                            "Count": rec_counts, "Date": np.tile(date, len(data)), "Plate": np.tile(plate, len(data))})
+                    print(df_add)
                     df_rec = df_rec.append(df_add)
 
     # write to csv
