f3561afa4dfc87588141e2971c67b7d2a9539260
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2021-04-12 14:16:38 -0700

    Fig4 polish (#792)
    * Still Selectivity
    
    * Still Selectivity
    
    * Change to Signaling
    
    * Fix 2, Final 4 edits
    
    * Make File

--- ckine/figures/figureC4.py
+++ ckine/figures/figureC4.py
@@ -29,8 +29,10 @@ def makeFigure():
 def cytBindingModelOpt(x, val, cellType, IL7=False):
     """Runs binding model for a given mutein, valency, dose, and cell type. """
     recQuantDF = pd.read_csv(join(path_here, "data/RecQuantitation.csv"))
+    convDict = pd.read_csv(join(path_here, "data/BindingConvDict.csv"))
     recCount = recQuantDF[["Receptor", cellType]]
     Kx = np.power(10, x[-1])
+
     if IL7:
         affs = [[np.power(10, x[0])]]
         recCount = [recCount.loc[(recCount.Receptor == "IL7Ra")][cellType].values]
@@ -41,6 +43,9 @@ def cytBindingModelOpt(x, val, cellType, IL7=False):
         recCount = [recCount.loc[(recCount.Receptor == "IL2Ra")][cellType].values, recCount.loc[(recCount.Receptor == "IL2Rb")][cellType].values]
         recCount = np.ravel(np.power(10, recCount))
         output = polyc(1e-9 / val, Kx, recCount, [[val, val]], [1.0], affs)[1][0][1]  # IL2RB binding only
+
+    if not IL7:
+        output *= np.mean(convDict.loc[(convDict.Cell == cellType)].Scale.values)
 
     return output
 
@@ -58,17 +63,18 @@ def minSelecFunc(x, val, targCell, offTCells, IL7=False):
 
 def optimizeDesign(ax, targCell, offTcells, IL7=False):
     """ A more general purpose optimizer """
+    vals = np.arange(1, 9, step=1)
+    sigDF = pd.DataFrame()
 
-    vals = np.logspace(0, 4, num=5, base=2)
     if IL7:
         optDF = pd.DataFrame(columns={"Valency", "Selectivity", "IL7Ra"})
         X0 = [8, -12]
-        optBnds = [(5, 11), (-13, -11)]  # Ka IL7, Kx
+        optBnds = [(6, 11), (-13, -11)]  # Ka IL7, Kx
     else:
         optDF = pd.DataFrame(columns={"Valency", "Selectivity", "IL2Ra", "IL2RBG"})
         X0 = [8, 8, -12]
-        optBnds = [(5, 11),  # Ka IL2Ra
-                   (5, 11),  # Ka IL2Rb
+        optBnds = [(6, 11),  # Ka IL2Ra
+                   (6, 11),  # Ka IL2Rb
                    (-13, -11)]  # Kx
 
     for val in vals:
@@ -78,25 +84,41 @@ def optimizeDesign(ax, targCell, offTcells, IL7=False):
         if IL7:
             IL7RaKD = 1e9 / np.power(10, optimized.x[0])
             optDF = optDF.append(pd.DataFrame({"Valency": [val], "Selectivity": [len(offTcells) / optimized.fun], "IL7Ra": IL7RaKD}))
+            sigDF = sigDF.append(pd.DataFrame({"Cell Type": [targCell[0]], "Target": ["Target"], "Valency": [val], "pSTAT": [cytBindingModelOpt(optimized.x, val, targCell[0], IL7)]}))
+            for cell in offTcells:
+                sigDF = sigDF.append(pd.DataFrame({"Cell Type": [cell], "Target": ["Off-Target"], "Valency": [val], "pSTAT": [cytBindingModelOpt(optimized.x, val, cell, IL7)]}))
         else:
             IL2RaKD = 1e9 / np.power(10, optimized.x[0])
             IL2RBGKD = 1e9 / np.power(10, optimized.x[1])
             optDF = optDF.append(pd.DataFrame({"Valency": [val], "Selectivity": [len(offTcells) / optimized.fun], "IL2Ra": IL2RaKD, "IL2RBG": IL2RBGKD}))
+            sigDF = sigDF.append(pd.DataFrame({"Cell Type": [targCell[0]], "Target": ["Target"], "Valency": [val], "pSTAT": [cytBindingModelOpt(optimized.x, val, targCell[0], IL7)]}))
+            for cell in offTcells:
+                sigDF = sigDF.append(pd.DataFrame({"Cell Type": [cell], "Target": ["Off-Target"], "Valency": [val], "pSTAT": [cytBindingModelOpt(optimized.x, val, cell, IL7)]}))
+
+    # Normalize to valency 1
+    for cell in targCell + offTcells:
+        sigDF.loc[sigDF["Cell Type"] == cell, "pSTAT"] = sigDF.loc[sigDF["Cell Type"] == cell, "pSTAT"].div(sigDF.loc[(sigDF["Cell Type"] == cell) & (sigDF.Valency == 1)].pSTAT.values[0])
 
     if IL7:
-        sns.barplot(x="Valency", y="Selectivity", data=optDF, ax=ax[0], palette="husl")
+        sns.lineplot(x="Valency", y="pSTAT", hue="Cell Type", style="Target", data=sigDF, ax=ax[0], palette="husl")
         ax[0].set(title=targCell[0] + " Selectivity with IL-7 mutein")
 
-        sns.barplot(x="Valency", y="IL7Ra", data=optDF, ax=ax[1], palette="crest")
+        sns.lineplot(x="Valency", y="IL7Ra", data=optDF, ax=ax[1], palette="crest")
         ax[1].set(yscale="log", ylabel=r"IL7·7Rα $K_D$ (nM)")
 
     else:
-        sns.barplot(x="Valency", y="Selectivity", data=optDF, ax=ax[0], palette="husl")
+        sns.lineplot(x="Valency", y="pSTAT", hue="Cell Type", style="Target", data=sigDF, ax=ax[0], palette="husl")
         ax[0].set(title=targCell[0] + " Selectivity with IL-2 mutein")
 
-        affDF = pd.melt(optDF, id_vars=['Valency'], value_vars=['IL2Ra', 'IL2RBG'])
-        affDF = affDF.rename(columns={"variable": "Receptor"})
-        sns.barplot(x="Valency", y="value", hue="Receptor", data=affDF, ax=ax[1])
-        ax[1].set(yscale="log", ylabel=r"IL2· $K_D$ (nM)")
+        if targCell == ["NK"]:
+            affDF = pd.melt(optDF, id_vars=['Valency'], value_vars=['IL2RBG'])
+            affDF = affDF.rename(columns={"variable": "Receptor"})
+            sns.lineplot(x="Valency", y="value", hue="Receptor", data=affDF, ax=ax[1])
+            ax[1].set(yscale="log", ylabel=r"IL2· $K_D$ (nM)")
+        else:
+            affDF = pd.melt(optDF, id_vars=['Valency'], value_vars=['IL2Ra', 'IL2RBG'])
+            affDF = affDF.rename(columns={"variable": "Receptor"})
+            sns.lineplot(x="Valency", y="value", hue="Receptor", data=affDF, ax=ax[1])
+            ax[1].set(yscale="log", ylabel=r"IL2· $K_D$ (nM)")
 
     return optimized
