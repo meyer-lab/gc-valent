f129caac3016c5ada668fa27079435a5c53d986f
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2022-08-19 14:12:06 -0700

    Actual Fix21

--- ckine/figures/figureC1.py
+++ ckine/figures/figureC1.py
@@ -24,7 +24,7 @@ cellDict = get_cellTypeDict()
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
-    ax, f = getSetup((6, 3), (1, 2), multz={0: 1, 4: 7})
+    ax, f = getSetup((15, 18), (6, 4), multz={0: 1, 4: 7})
     axlabel = copy(ax)
     del axlabel[1]
     subplotLabel(axlabel)
