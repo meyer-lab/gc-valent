3bfd826c7d4ee1e4a1bd421e3dbec003325aa9e0
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2022-10-10 15:43:50 -0700

    Add S7

---
+++ ckine/figures/figureS7.py
@@ -0,0 +1,28 @@
+"""
+This creates Figure S7, assymmetric results.
+"""
+
+import os
+import matplotlib.pyplot as plt
+from .figureCommon import subplotLabel, getSetup, getLigDict, ligandPlot, ligand_ratio_plot
+from ..MBmodel import runFullModelMeyer
+
+path_here = os.path.dirname(os.path.dirname(__file__))
+ligDict = getLigDict()
+plt.rcParams['svg.fonttype'] = 'none'
+
+
+def makeFigure():
+    """Get a list of the axis objects and create a figure"""
+
+    ax, f = getSetup((8, 2), (1, 4))
+    subplotLabel(ax)
+
+    modelDF = runFullModelMeyer().reset_index()
+
+    ligandPlot(modelDF, "Treg", ax[0], live_dead=True)
+    ligandPlot(modelDF, "Thelper", ax[1], live_dead=True)
+    ligandPlot(modelDF, "NK", ax[2], live_dead=True)
+    ligandPlot(modelDF, "CD8", ax[3], live_dead=True)
+
+    return f
