4cfb6f3dec458f85680c59f90a003966dd2a9e78
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2023-03-27 16:08:01 -0700

    remove FCS (#941)

--- ckine/flow_meyer.py
+++ ckine/flow_meyer.py
@@ -237,3 +237,121 @@ def make_flow_df(subtract=True):
             MeyerDF.pSTAT5 = MeyerDF.pSTAT5.clip(lower=0)
     MeyerDF.to_csv(join(path_here, "ckine/data/Meyer_Flow.csv"))
     return MeyerDF
+
+
+flow_dict_ILC = {"Pacific Blue-A": "FoxP3",
+"FITC-A": "Lin",
+"Alexa Fluor 647-A": "FoxP3",
+"BV605-A": "CRTH2",
+"APC-Cy7-A": "CD25",
+"BV421-A": "CD127"}
+
+
+def combineWells_ILC(samples, cellFrac):
+    """Accepts sample array returned from importF, and array of channels, returns combined well data"""
+    comp_mat = compMatrix(ILCs=True)
+    markers = np.array(["Pacific Blue-A", "FITC-A", "B421-A", "BV605-A", "APC-Cy7-A", "Alexa Fluor 647-A"])
+    log_markers = markers[np.isin(markers, samples[0].data.columns)]
+    samples[0] = applyMatrix(samples[0], comp_mat)
+    samples[0] = samples[0].transform("tlog", channels=log_markers)
+    combinedSamples = samples[0]
+    for sample in samples[1:]:
+        log_markers = markers[np.isin(markers, sample.data.columns)]
+        sample = applyMatrix(sample, comp_mat)
+        sample = sample.transform("tlog", channels=log_markers)
+        combinedSamples.data = pd.concat([combinedSamples.data, sample.data.sample(frac=cellFrac)])
+    
+    combinedSamples.data = combinedSamples.data.rename(flow_dict_ILC, axis=1)
+    combinedSamples.data = combinedSamples.data.astype(int)
+    combinedSamples.data = combinedSamples.data.clip(lower=0, upper=5e5)
+    return combinedSamples
+
+
+def compMatrix_ILCs(invert=True):
+    """Creates compensation matrix given parameters date in mm-dd, plate number and panel A, B, or C."""
+    path = join(path_here, "ckine/data/Flow_Data_Meyer/ILCs/Compensation.csv")
+    # imports csv file with comp values as a dataframe
+    header_names = ['Channel1', 'Channel2', 'Comp']
+    df_comp = pd.read_csv(path, header=None, index_col=False, skiprows=0, names=header_names)
+    # Add diangonal values of 100 to compensation values
+    addedChannels = []
+    for i in df_comp.index:
+        channelName = df_comp.iloc[i]['Channel1']
+        if channelName not in addedChannels:  # Ensures a diagonal value is only added once for each channel
+            addedChannels.append(channelName)
+            df2 = pd.DataFrame([[channelName, channelName, 100]], columns=['Channel1', 'Channel2', 'Comp'])  # Creates new row for dataframe
+            df_comp = pd.concat([df_comp, df2], ignore_index=True)  # Adds row
+    # Create square matrix from compensation values
+    df_matrix = pd.DataFrame(index=addedChannels, columns=addedChannels)  # df_matrix is now a square and has exactly one row and one column for each channel
+    for i in df_matrix.index:
+        for c in df_matrix.columns:
+            df_matrix.at[i, c] = df_comp.loc[(df_comp['Channel1'] == c) & (df_comp['Channel2'] == i), 'Comp'].iloc[0]  # Fills in square matrix by finding corresponding comp value from csv
+    # df_matrix now has all values in square matrix form
+    df_matrix = df_matrix.div(100)
+    if invert:  # true by default, inverts matrix before returning it
+        a = np.matrix(df_matrix.values, dtype=float)  # Convert to np to allow for linalg usage
+        df_matrix = pd.DataFrame(np.linalg.inv(a), df_matrix.columns, df_matrix.index)  # Calculate inverse and put pack as dataframe
+    return df_matrix
+
+
+def process_sample_ILCs(sample):
+    """Relabels and logs a sample"""
+    comp_mat = compMatrix_ILCs()
+    sample = applyMatrix(sample, comp_mat)
+    markers = np.array(["Pacific Blue-A", "FITC-A", "B421-A", "BV605-A", "APC-Cy7-A", "Alexa Fluor 647-A"])
+    log_markers = markers[np.isin(markers, sample.data.columns)]
+    sample = sample.transform("tlog", channels=log_markers)
+    sample.data = sample.data.rename(flow_dict_ILC, axis=1)
+    return sample, log_markers
+
+
+gate_dict_ILCs = {
+             "Treg": ["T Gate", "Treg Gate"],
+             "ILC2": ["ILC Gate", "ILC2 Gate"]}
+
+
+def live_PBMC_gate_ILCs(sample, gateDF, donor):
+    """Returns singlet lymphocyte live PBMCs for a patient"""
+    gates = ["PBMC Gate", "Live Gate"]
+    gateDF = gateDF.loc[gateDF.Donor == donor]
+    for gate_name in gates:
+        gate = form_gate(gateDF.loc[(gateDF["Gate Label"] == gate_name)].Gate.values[0])
+        sample = sample.gate(gate)
+    return sample
+
+
+def pop_gate_ILCs(sample, cell_type, gateDF, donor):
+    """Extracts cell population sample"""
+    gateDF = gateDF.loc[gateDF.Donor == donor]
+    gates = gate_dict_ILCs[cell_type]
+    for gate_name in gates:
+        gate = form_gate(gateDF.loc[(gateDF["Gate Label"] == gate_name)].Gate.values[0])
+        pop_sample = sample.gate(gate)
+    return pop_sample
+
+
+def make_flow_df_ILCs(subtract=True):
+    """Compiles data for all populations for all patients into .csv"""
+    donors = ["Donor_303", "Donor_334", "Donor_850", "Donor_X"]
+    donorFormat = ["Donor 303", "Donor 334", "Donor 850", "Donor X"]
+
+    cell_types = ["Treg", "ILC2"]
+    gateDF = pd.read_csv(join(path_here, "ckine/data/Meyer_Flow_Gates_ILCs.csv"))
+    MeyerDF = pd.DataFrame([])
+
+    for i, donor in enumerate(donors):
+        print(donor)
+        sample = FCMeasurement(ID=donor, datafile=(join(path_here, "ckine/data/Flow_Data_Meyer/ILCs/" + donor +  "/Specimen_001_" + donor + ".fcs")))
+        sample, _ = process_sample_ILCs(sample)
+        sample = live_PBMC_gate_ILCs(sample, gateDF, donorFormat[i].split(" ")[-1])
+        for cell_type in cell_types:
+            pop_sample = pop_gate_ILCs(sample, cell_type, gateDF, donorFormat[i].split(" ")[-1])
+            mean = pop_sample.data["CD25"]
+            if len(mean) == 0:
+                mean = 0
+            else:
+                mean = np.mean(mean.values[mean.values < np.quantile(mean.values, 0.995)])
+            MeyerDF = pd.concat([MeyerDF, pd.DataFrame({ "Cell": cell_type, "CD25": [mean], "Donor": donorFormat[i]})])
+
+    MeyerDF.to_csv(join(path_here, "ckine/data/Meyer_Flow_ILCs.csv"))
+    return MeyerDF
